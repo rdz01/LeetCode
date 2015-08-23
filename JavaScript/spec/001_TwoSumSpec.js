34e3deece23d332ec8ddee7c155e2c5ded533900
BigEgg <cd_bigegg@hotmail.com>
2015-08-23 15:10:20 +0800

    Add the JavaScript solution for Problem #1

---
+++ JavaScript/spec/001_TwoSumSpec.js
@@ -0,0 +1,94 @@
+var solution = require('../src/001_TwoSum');
+
+describe('Two Sum Tests', function() {
+    'use strict';
+
+    var largeArray = new Array(20000);
+
+    beforeAll(function() {
+        for (var i = 0; i < largeArray.length; i++) {
+            largeArray[i] = i * 2;
+        }
+    });
+
+    it('should get result when array is ordered', function() {
+        var nums = [
+            2, 7, 11, 15
+        ];
+        var target = 9;
+
+        var result = solution.twoSum(nums, target);
+
+        expect(result.length).toEqual(2);
+        expect(result[0]).toEqual(1);
+        expect(result[1]).toEqual(2);
+    });
+
+    it('should get result when array is unordered', function() {
+        var nums = [
+            5, 75, 25
+        ];
+        var target = 100;
+
+        var result = solution.twoSum(nums, target);
+
+        expect(result.length).toEqual(2);
+        expect(result[0]).toEqual(2);
+        expect(result[1]).toEqual(3);
+    });
+
+    it('should get result when array has duplicate numbers', function() {
+        var nums = [
+            5, 5, 15, 30
+        ];
+        var target = 20;
+
+        var result = solution.twoSum(nums, target);
+
+        expect(result.length).toEqual(2);
+        expect(result[0]).toEqual(2);
+        expect(result[1]).toEqual(3);
+    });
+
+    it('should get result when array\'s items are all the same', function() {
+        var nums = [
+            5, 5, 5, 5
+        ];
+        var target = 10;
+
+        var result = solution.twoSum(nums, target);
+
+        expect(result.length).toEqual(2);
+        expect(result[0]).toEqual(1);
+        expect(result[1]).toEqual(2);
+    });
+
+    it('should not get result when non of the sum is the target', function() {
+        var nums = [
+            2, 7, 11, 15
+        ];
+        var target = 5;
+
+        var result = solution.twoSum(nums, target);
+
+        expect(result.length).toEqual(0);
+    });
+
+    it('should get result even the array is large', function() {
+        var target = 19082;
+
+        var result = solution.twoSum(largeArray, target);
+
+        expect(result.length).toEqual(2);
+        expect(result[0]).toEqual(4771);
+        expect(result[1]).toEqual(4772);
+    });
+
+    it('should not get result when non of the sum is the target, even the array is large', function() {
+        var target = 19081;
+
+        var result = solution.twoSum(largeArray, target);
+
+        expect(result.length).toEqual(0);
+    });
+});
---
+++ JavaScript/src/001_TwoSum.js
@@ -0,0 +1,66 @@
+var solution = function() {
+    'use strict';
+
+    var keys = [];
+    var values = [];
+
+    var hash = function(key, size) {
+        return (key & 0x7FFFFFFF) % size;
+    };
+
+    var initialize = function(size) {
+        keys = new Array(size);
+        values = new Array(size);
+
+        for (var i = 0; i < size; i++) {
+            keys[i] = -1;
+        }
+    };
+
+    var put = function(key, value, size) {
+        var i = 0;
+        for (i = hash(key, size); keys[i] != -1; i = (i + 1) % size) {
+            if (keys[i] == key) {
+                values[i] = value;
+            }
+        }
+        keys[i] = key;
+        values[i] = value;
+    };
+
+    var get = function(key, size) {
+        for (var i = hash(key, size); keys[i] != -1; i = (i + 1) % size) {
+            if (keys[i] == key) {
+                return values[i];
+            }
+        }
+        return -1;
+    };
+
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    var twoSum = function(nums, target) {
+        initialize(nums.length);
+
+        for (var i = 0; i < nums.length; i++) {
+            if (get(nums[i], nums.length) == -1) {
+                put(target - nums[i], i, nums.length);
+            } else {
+                var result = [];
+                result[0] = get(nums[i], nums.length) + 1;
+                result[1] = i + 1;
+                return result;
+            }
+        }
+        return [];
+    };
+
+    return {
+        twoSum: twoSum
+    };
+};
+
+module.exports = solution();
