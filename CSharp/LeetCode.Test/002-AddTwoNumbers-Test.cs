148e953dc06d9a578556417764cc1c69e9ea0abc
BigEgg <cd_bigegg@hotmail.com>
2015-08-23 17:23:00 +0800

    Add C# solution for problem #2

---
+++ CSharp/LeetCode.Test/002-AddTwoNumbers-Test.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _002_AddTwoNumbers_Test
+    {
+        [TestMethod]
+        public void AddTwoNumbersTest_General()
+        {
+            var link = GenerateList(new int[] { 2, 4, 3 });
+
+            var solution = new _002_AddTwoNumbers();
+            var result = solution.AddTwoNumbers(link, link);
+
+            AssertList(result, new int[] { 4, 8, 6 });
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTest_General_2()
+        {
+            var link = GenerateList(new int[] { 0 });
+
+            var solution = new _002_AddTwoNumbers();
+            var result = solution.AddTwoNumbers(link, link);
+
+            AssertList(result, new int[] { 0 });
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTest_HasCarry()
+        {
+            var link1 = GenerateList(new int[] { 2, 4, 3 });
+            var link2 = GenerateList(new int[] { 5, 6, 4 });
+
+            var solution = new _002_AddTwoNumbers();
+            var result = solution.AddTwoNumbers(link1, link2);
+
+            AssertList(result, new int[] { 7, 0, 8 });
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTest_HasMultipleCarry()
+        {
+            var link1 = GenerateList(new int[] { 1 });
+            var link2 = GenerateList(new int[] { 9, 9 });
+
+            var solution = new _002_AddTwoNumbers();
+            var result = solution.AddTwoNumbers(link1, link2);
+
+            AssertList(result, new int[] { 0, 0, 1 });
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTest_FirstNumCarry()
+        {
+            var link1 = GenerateList(new int[] { 3, 4, 4 });
+            var link2 = GenerateList(new int[] { 3, 4, 6 });
+
+            var solution = new _002_AddTwoNumbers();
+            var result = solution.AddTwoNumbers(link1, link2);
+
+            AssertList(result, new int[] { 6, 8, 0, 1 });
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTest_L1Longger()
+        {
+            var link1 = GenerateList(new int[] { 2, 4, 3, 1 });
+            var link2 = GenerateList(new int[] { 5, 6, 4 });
+
+            var solution = new _002_AddTwoNumbers();
+            var result = solution.AddTwoNumbers(link1, link2);
+
+            AssertList(result, new int[] { 7, 0, 8, 1 });
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTest_L1Longger_2()
+        {
+            var link1 = GenerateList(new int[] { 1, 8 });
+            var link2 = GenerateList(new int[] { 0 });
+
+            var solution = new _002_AddTwoNumbers();
+            var result = solution.AddTwoNumbers(link1, link2);
+
+            AssertList(result, new int[] { 1, 8 });
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTest_L1Empty()
+        {
+            var link1 = GenerateList(null);
+            var link2 = GenerateList(new int[] { 1, 8 });
+
+            var solution = new _002_AddTwoNumbers();
+            var result = solution.AddTwoNumbers(link1, link2);
+
+            AssertList(result, new int[] { 1, 8 });
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTest_L2Longger()
+        {
+            var link1 = GenerateList(new int[] { 2, 4, 3 });
+            var link2 = GenerateList(new int[] { 5, 6, 4, 3 });
+
+            var solution = new _002_AddTwoNumbers();
+            var result = solution.AddTwoNumbers(link1, link2);
+
+            AssertList(result, new int[] { 7, 0, 8, 3 });
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTest_L2Longger_2()
+        {
+            var link1 = GenerateList(new int[] { 0 });
+            var link2 = GenerateList(new int[] { 1, 8 });
+
+            var solution = new _002_AddTwoNumbers();
+            var result = solution.AddTwoNumbers(link1, link2);
+
+            AssertList(result, new int[] { 1, 8 });
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTest_L2Empty()
+        {
+            var link1 = GenerateList(new int[] { 1, 8 });
+            var link2 = GenerateList(null);
+
+            var solution = new _002_AddTwoNumbers();
+            var result = solution.AddTwoNumbers(link1, link2);
+
+            AssertList(result, new int[] { 1, 8 });
+        }
+
+        private ListNode GenerateList(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) { return null; }
+
+            var i = 0;
+            var first = new ListNode(nums[i]);
+            var current = first;
+
+            while (++i < nums.Length)
+            {
+                current.next = new ListNode(nums[i]);
+                current = current.next;
+            }
+
+            return first;
+        }
+
+        private void AssertList(ListNode first, int[] nums)
+        {
+            Assert.IsNotNull(first);
+            Assert.IsNotNull(nums);
+            Assert.IsTrue(nums.Length > 0);
+
+            var current = first;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Assert.IsNotNull(current);
+                Assert.AreEqual(nums[i], current.val);
+                current = current.next;
+            }
+
+            Assert.IsNull(current);
+        }
+    }
+}
---
+++ CSharp/LeetCode/002-AddTwoNumbers.cs
@@ -0,0 +1,69 @@
+﻿namespace LeetCode
+{
+    public class _002_AddTwoNumbers
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var current = new ListNode(0);
+            ListNode first = current;
+            ListNode previous = null;
+
+            while (l1 != null && l2 != null)
+            {
+                current = CalculateCurrent(current, l1.val, l2.val);
+                previous = current;
+                current = current.next;
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            while (l1 != null)
+            {
+                current = CalculateCurrent(current, l1.val, 0);
+                previous = current;
+                current = current.next;
+                l1 = l1.next;
+            }
+
+            while (l2 != null)
+            {
+                current = CalculateCurrent(current, 0, l2.val);
+                previous = current;
+                current = current.next;
+                l2 = l2.next;
+            }
+
+            if (current.val == 0)
+            {
+                previous.next = null;
+            }
+
+            return first;
+        }
+
+        private ListNode CalculateCurrent(ListNode current, int value1, int value2)
+        {
+            var sum = value1 + value2 + current.val;
+
+            if (sum >= 10)
+            {
+                current.val = sum % 10;
+                current.next = new ListNode(1);
+            }
+            else
+            {
+                current.val = sum;
+                current.next = new ListNode(0);
+            }
+
+            return current;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
