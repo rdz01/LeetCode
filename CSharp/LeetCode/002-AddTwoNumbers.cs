e33520d35aa35d7ce03269cc4e58e7b739291c67
BigEgg <cd_bigegg@hotmail.com>
2015-08-26 21:25:57 +0800

    Optimize the C# solution for problem #2

--- CSharp/LeetCode/002-AddTwoNumbers.cs
+++ CSharp/LeetCode/002-AddTwoNumbers.cs
@@ -4,59 +4,39 @@
     {
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            var current = new ListNode(0);
-            ListNode first = current;
-            ListNode previous = null;
+            var first = new ListNode(-1);
+            first.next = new ListNode(0);
+            var current = first;
 
-            while (l1 != null && l2 != null)
+            while (l1 != null || l2 != null)
             {
-                current = CalculateCurrent(current, l1.val, l2.val);
-                previous = current;
+                var value1 = l1 == null ? 0 : l1.val;
+                var value2 = l2 == null ? 0 : l2.val;
+
                 current = current.next;
-                l1 = l1.next;
-                l2 = l2.next;
+                var sum = value1 + value2 + current.val;
+
+                if (sum >= 10)
+                {
+                    current.val = sum % 10;
+                    current.next = new ListNode(1);
+                }
+                else
+                {
+                    current.val = sum;
+                    current.next = new ListNode(0);
+                }
+
+                l1 = l1 == null ? null : l1.next;
+                l2 = l2 == null ? null : l2.next;
             }
 
-            while (l1 != null)
+            if (current.next.val == 0)
             {
-                current = CalculateCurrent(current, l1.val, 0);
-                previous = current;
-                current = current.next;
-                l1 = l1.next;
+                current.next = null;
             }
 
-            while (l2 != null)
-            {
-                current = CalculateCurrent(current, 0, l2.val);
-                previous = current;
-                current = current.next;
-                l2 = l2.next;
-            }
-
-            if (current.val == 0)
-            {
-                previous.next = null;
-            }
-
-            return first;
-        }
-
-        private ListNode CalculateCurrent(ListNode current, int value1, int value2)
-        {
-            var sum = value1 + value2 + current.val;
-
-            if (sum >= 10)
-            {
-                current.val = sum % 10;
-                current.next = new ListNode(1);
-            }
-            else
-            {
-                current.val = sum;
-                current.next = new ListNode(0);
-            }
-
-            return current;
+            return first.next;
         }
     }
 
