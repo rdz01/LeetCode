920128dd4baab96c621f7f42caa5dd97586f42d9
BigEgg <cd_bigegg@hotmail.com>
2015-08-27 23:23:31 +0800

    Add the C++ solution for problem #3

---
+++ C++/LeetCode/003-LongestSubstringWithoutRepeatingCharacters.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string.h>
+#include <unordered_map>
+#include "common.h"
+using namespace std;
+
+class LEETCODE_API _003_LongestSubstringWithoutRepeatingCharacters
+{
+public:
+	_003_LongestSubstringWithoutRepeatingCharacters();
+	~_003_LongestSubstringWithoutRepeatingCharacters();
+
+	int lengthOfLongestSubstring(string s);
+};
+
