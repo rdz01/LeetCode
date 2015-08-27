94ce11a5798bc2d778e487bd71c43978095168b3
BigEgg <cd_bigegg@hotmail.com>
2015-08-27 22:30:51 +0800

    Add C# solution for problem #3

---
+++ CSharp/LeetCode.Test/003-LongestSubstringWithoutRepeatingCharacters-Test.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _003_LongestSubstringWithoutRepeatingCharacters_Test
+    {
+        [TestMethod]
+        public void GeneralTest()
+        {
+            var input = "abcabcbb";
+
+            var solution = new _003_LongestSubstringWithoutRepeatingCharacters();
+            var result = solution.LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void RepeatCharsTest()
+        {
+            var input = "bbbbb";
+
+            var solution = new _003_LongestSubstringWithoutRepeatingCharacters();
+            var result = solution.LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void EmptyInputTest()
+        {
+            var solution = new _003_LongestSubstringWithoutRepeatingCharacters();
+            var result = solution.LengthOfLongestSubstring("");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void LongStringTest()
+        {
+            var input = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+
+            var solution = new _003_LongestSubstringWithoutRepeatingCharacters();
+            var result = solution.LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(26, result);
+        }
+
+        [TestMethod]
+        public void LongStringTest_HaveShortRepeat()
+        {
+            var input = "abcdefghijklmnopqrstuvwxyzabcdefghijk  lmnopqrstuvwxyz";
+
+            var solution = new _003_LongestSubstringWithoutRepeatingCharacters();
+            var result = solution.LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(27, result);
+        }
+    }
+}
