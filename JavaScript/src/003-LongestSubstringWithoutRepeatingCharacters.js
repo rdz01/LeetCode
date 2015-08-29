af061f5ebbbd8cef65437efe5209b11a54c7cdf9
BigEgg <cd_bigegg@hotmail.com>
2015-08-29 23:02:27 +0800

    Add the JavaScript solution for problem #3

---
+++ JavaScript/src/003-LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,40 @@
+var solution = function() {
+    'use strict';
+
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    var lengthOfLongestSubstring = function(s) {
+        if (!s || s.lenght === 0) {
+            return 0;
+        }
+
+        var i = 0;
+        var map = new Array(256);
+        for (i = 0; i < 256; i++) {
+            map[i] = -1;
+        }
+
+        var maxLen = 0;
+        var lastRepeatPos = -1;
+
+        for (i = 0; i < s.length; i++) {
+            if (map[s[i]] !== -1 && lastRepeatPos < map[s[i]]) {
+                lastRepeatPos = map[s[i]];
+            }
+            if (maxLen < i - lastRepeatPos) {
+                maxLen = i - lastRepeatPos;
+            }
+            map[s[i]] = i;
+        }
+
+        return maxLen;
+    };
+
+    return {
+        lengthOfLongestSubstring: lengthOfLongestSubstring
+    };
+};
+
+module.exports = solution();
