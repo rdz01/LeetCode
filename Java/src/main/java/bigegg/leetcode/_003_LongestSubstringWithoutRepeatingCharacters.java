421a8505bb899939197c34ed94f2fad637af55e5
BigEgg <cd_bigegg@hotmail.com>
2015-08-29 22:13:34 +0800

    Optimize the Java solution for problem #3

--- Java/src/main/java/bigegg/leetcode/_003_LongestSubstringWithoutRepeatingCharacters.java
+++ Java/src/main/java/bigegg/leetcode/_003_LongestSubstringWithoutRepeatingCharacters.java
@@ -1,25 +1,26 @@
 package bigegg.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class _003_LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
 
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        int[] map = new int[256];
+        for (int i = 0; i < 256; i++) {
+            map[i] = -1;
+        }
+
         int maxLen = 0;
         int lastRepeatPos = -1;
         for (int i = 0; i < s.length(); i++) {
-            if (map.containsKey(s.charAt(i)) && lastRepeatPos < map.get(s.charAt(i))) {
-                lastRepeatPos = map.get(s.charAt(i));
+            if (map[s.charAt(i)] != -1 && lastRepeatPos < map[s.charAt(i)]) {
+                lastRepeatPos = map[s.charAt(i)];
             }
             if (maxLen < i - lastRepeatPos) {
                 maxLen = i - lastRepeatPos;
             }
-            map.put(s.charAt(i), i);
+            map[s.charAt(i)] = i;
         }
 
         return maxLen;
