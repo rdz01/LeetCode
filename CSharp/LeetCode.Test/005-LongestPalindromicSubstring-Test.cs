75ca1b7b5bba5a94a485008eeefc55197f048cc2
BigEgg <cd_bigegg@hotmail.com>
2015-08-30 12:55:12 +0800

    Add the C# solution for problem #5

---
+++ CSharp/LeetCode.Test/005-LongestPalindromicSubstring-Test.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _005_LongestPalindromicSubstring_Test
+    {
+        [TestMethod]
+        public void GeneralTest_even()
+        {
+            var input = "abcdefgfedcba";
+
+            var solution = new _005_LongestPalindromicSubstring();
+            var result = solution.LongestPalindrome(input);
+
+            Assert.AreEqual("abcdefgfedcba", result);
+        }
+
+        [TestMethod]
+        public void GeneralTest_odd()
+        {
+            var input = "abcdefggfedcba";
+
+            var solution = new _005_LongestPalindromicSubstring();
+            var result = solution.LongestPalindrome(input);
+
+            Assert.AreEqual("abcdefggfedcba", result);
+        }
+
+        [TestMethod]
+        public void EmptyStringTest()
+        {
+            var input = "";
+
+            var solution = new _005_LongestPalindromicSubstring();
+            var result = solution.LongestPalindrome(input);
+
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void OneCharacterStringTest()
+        {
+            var input = "a";
+
+            var solution = new _005_LongestPalindromicSubstring();
+            var result = solution.LongestPalindrome(input);
+
+            Assert.AreEqual("a", result);
+        }
+
+        [TestMethod]
+        public void MultiplePalindrome()
+        {
+            var input = "aabccdccbaaeeggee";
+
+            var solution = new _005_LongestPalindromicSubstring();
+            var result = solution.LongestPalindrome(input);
+
+            Assert.AreEqual("aabccdccbaa", result);
+        }
+    }
+}
---
+++ CSharp/LeetCode/005-LongestPalindromicSubstring.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode
+{
+    public class _005_LongestPalindromicSubstring
+    {
+        public string LongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length == 1) { return s; }
+
+            var longestPalindrome = "";
+            var longestPalindromeLength = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var palindrome = GetPalindromeString(s, i, i);
+                if (palindrome.Length > longestPalindromeLength)
+                {
+                    longestPalindrome = palindrome;
+                    longestPalindromeLength = longestPalindrome.Length;
+                }
+                palindrome = GetPalindromeString(s, i, i + 1);
+                if (palindrome.Length > longestPalindromeLength)
+                {
+                    longestPalindrome = palindrome;
+                    longestPalindromeLength = longestPalindrome.Length;
+                }
+            }
+
+            return longestPalindrome;
+        }
+
+        string GetPalindromeString(string s, int left, int right)
+        {
+            var hasPalindrome = false;
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                hasPalindrome = true;
+                left--;
+                right++;
+            }
+            return hasPalindrome ? s.Substring(left + 1, right - left - 1) : "";
+        }
+    }
+}
