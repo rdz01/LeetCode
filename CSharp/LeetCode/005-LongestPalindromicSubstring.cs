a9382aad83e1733b6b8999deca296ae031ce6448
BigEgg <cd_bigegg@hotmail.com>
2015-09-18 01:25:43 +0800

    Use Manacher's algorithm to rewrite C# solution for problem #5

--- CSharp/LeetCode/005-LongestPalindromicSubstring.cs
+++ CSharp/LeetCode/005-LongestPalindromicSubstring.cs
@@ -2,41 +2,55 @@
 {
     public class _005_LongestPalindromicSubstring
     {
+        int Min(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+
         public string LongestPalindrome(string s)
         {
             if (string.IsNullOrEmpty(s) || s.Length == 1) { return s; }
 
-            var longestPalindrome = "";
-            var longestPalindromeLength = 0;
-            for (int i = 0; i < s.Length - longestPalindromeLength / 2; i++)
+            int n2 = s.Length * 2 + 1;
+            var s2 = new char[n2];
+            for (int i = 0; i < s.Length; i++)
             {
-                var palindrome = GetPalindromeString(s, i, i);
-                if (palindrome.Length > longestPalindromeLength)
+                s2[i * 2] = '#';
+                s2[i * 2 + 1] = s[i];
+            }
+            s2[n2 - 1] = '#';
+
+            var p = new int[n2];
+            var range_max = 0;
+            var center = 0;
+            var longestPalindromeCenter = 0;
+
+            for (int i = 1; i < n2 - 1; i++)
+            {
+                if (range_max > i)
                 {
-                    longestPalindrome = palindrome;
-                    longestPalindromeLength = longestPalindrome.Length;
+                    p[i] = p[center * 2 - i] < range_max - i ? p[center * 2 - i] : range_max - i;
                 }
-                palindrome = GetPalindromeString(s, i, i + 1);
-                if (palindrome.Length > longestPalindromeLength)
+
+                while (i - 1 - p[i] >= 0 && i + 1 + p[i] < n2 && s2[i - 1 - p[i]] == s2[i + 1 + p[i]])
                 {
-                    longestPalindrome = palindrome;
-                    longestPalindromeLength = longestPalindrome.Length;
+                    p[i]++;
+                }
+
+                if (i + p[i] > range_max)
+                {
+                    range_max = i + p[i];
+                    center = i;
+                }
+
+                if (p[i] > p[longestPalindromeCenter])
+                {
+                    longestPalindromeCenter = i;
                 }
             }
 
-            return longestPalindrome;
-        }
-
-        string GetPalindromeString(string s, int left, int right)
-        {
-            var hasPalindrome = false;
-            while (left >= 0 && right < s.Length && s[left] == s[right])
-            {
-                hasPalindrome = true;
-                left--;
-                right++;
-            }
-            return hasPalindrome ? s.Substring(left + 1, right - left - 1) : "";
+            var range = p[longestPalindromeCenter];
+            return s.Substring((longestPalindromeCenter - range) / 2, range);
         }
     }
 }
