87e78a19f61b2b96a0e2e56bdf0506540b3e7c0a
BigEgg <cd_bigegg@hotmail.com>
2015-09-21 00:01:34 +0800

    #4 Add C# solution

---
+++ CSharp/LeetCode.Test/004-MedianOfTwoSortedArrays-Test.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _004_MedianOfTwoSortedArrays_Test
+    {
+        [TestMethod]
+        public void FindMedianSortedArraysTest_General_Odd()
+        {
+            int[] nums1 = { 1, 2, 3 };
+            int[] nums2 = { 3, 4, 5 };
+
+            var solution = new _004_MedianOfTwoSortedArrays();
+            var result = solution.FindMedianSortedArrays(nums1, nums2);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void FindMedianSortedArraysTest_General_Even()
+        {
+            int[] nums1 = { 1, 2, 3, 4 };
+            int[] nums2 = { 5, 6, 7, 8 };
+
+            var solution = new _004_MedianOfTwoSortedArrays();
+            var result = solution.FindMedianSortedArrays(nums1, nums2);
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void FindMedianSortedArraysTest_Num1Empty()
+        {
+            int[] nums1 = { };
+            int[] nums2 = { 1, 2, 3 };
+
+            var solution = new _004_MedianOfTwoSortedArrays();
+            var result = solution.FindMedianSortedArrays(nums1, nums2);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void FindMedianSortedArraysTest_Num2Empty()
+        {
+            int[] nums1 = { 1, 2, 3 };
+            int[] nums2 = { };
+
+            var solution = new _004_MedianOfTwoSortedArrays();
+            var result = solution.FindMedianSortedArrays(nums1, nums2);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void FindMedianSortedArraysTest_Num1Empty_Nums2SingleItem()
+        {
+            int[] nums1 = { };
+            int[] nums2 = { 1 };
+
+            var solution = new _004_MedianOfTwoSortedArrays();
+            var result = solution.FindMedianSortedArrays(nums1, nums2);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void FindMedianSortedArraysTest_Num2Empty_Nums1SingleItem()
+        {
+            int[] nums1 = { 1 };
+            int[] nums2 = { };
+
+            var solution = new _004_MedianOfTwoSortedArrays();
+            var result = solution.FindMedianSortedArrays(nums1, nums2);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void FindMedianSortedArraysTest_MixedArray()
+        {
+            int[] nums1 = { 1, 2, 3, 4 };
+            int[] nums2 = { 3, 4, 5, 5, 6 };
+
+            var solution = new _004_MedianOfTwoSortedArrays();
+            var result = solution.FindMedianSortedArrays(nums1, nums2);
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ CSharp/LeetCode/004-MedianOfTwoSortedArrays.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode
+{
+    public class _004_MedianOfTwoSortedArrays
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            return FindKth(nums1, 0, nums2, 0, (nums1.Length + nums2.Length) / 2);
+        }
+
+        double FindKth(int[] nums1, int startIndex1, int[] nums2, int startIndex2, int k)
+        {
+            if (nums1.Length - startIndex1 < nums2.Length - startIndex2) { return FindKth(nums2, startIndex2, nums1, startIndex1, k); }
+            if (nums2.Length <= startIndex2) { return nums1[k]; }
+            if (k == 1) { return nums1[startIndex1] < nums2[startIndex2] ? nums1[startIndex1] : nums2[startIndex2]; }
+
+            var index1 = k / 2 + startIndex1 < nums1.Length - startIndex1 ? k / 2 : nums1.Length - startIndex1;
+            var index2 = k - index1 + startIndex2;
+            if (nums1[index1] > nums2[index2])
+            {
+                return FindKth(nums1, startIndex1, nums2, index2, k - index2 + startIndex2);
+            }
+            else if (nums1[index1] < nums2[index2])
+            {
+                return FindKth(nums1, index1, nums2, startIndex2, k - index1 + startIndex1);
+            }
+            else
+            {
+                return nums1[index1 - 1];
+            }
+        }
+    }
+}
