bf128953c3ec5b4319585e6aff6114f616b5e542
BigEgg <cd_bigegg@hotmail.com>
2015-10-27 21:35:17 +0800

    #9 Add C# version solution

---
+++ CSharp/LeetCode/009-PalindromeNumber.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode
+{
+    public class _009_PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0) { return false; }
+
+            var num = 1;
+            while (x / num >= 10) { num *= 10; }
+
+            while (x > 0)
+            {
+                var firstNumber = x / num;
+                var lastNumber = x % 10;
+
+                if (firstNumber != lastNumber) { return false; }
+
+                x = x % num / 10;
+                num /= 100;
+            }
+
+            return true;
+        }
+    }
+}
