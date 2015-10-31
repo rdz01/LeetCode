2144869f6678f713b0cf878ffc8b0dd702d06c81
BigEgg <cd_bigegg@hotmail.com>
2015-10-31 14:06:14 +0800

    #11 Add C# version solution

---
+++ CSharp/LeetCode/011-ContainerWithMostWater.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode
+{
+    public class _011_ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            var start = 0;
+            var end = height.Length - 1;
+            var result = 0;
+            var area = 0;
+
+            while (start < end)
+            {
+                area = (height[start] < height[end] ? height[start] : height[end]) * (end - start);
+                result = result < area ? area : result;
+                if (height[start] <= height[end])
+                {
+                    start++;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+            return result;
+        }
+    }
+}
