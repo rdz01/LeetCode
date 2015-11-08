215e23d0caf89fb9d18f83b8bac32cca890e7f5b
BigEgg <cd_bigegg@hotmail.com>
2015-11-08 22:09:13 +0800

    #22 Add C# solution

---
+++ CSharp/LeetCode/022-GenerateParentheses.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class _022_GenerateParentheses
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var results = new List<string>();
+            if (n <= 0) { return results; }
+
+            GenerateParenthesis(n, n, results, "");
+            return results;
+        }
+
+        void GenerateParenthesis(int l, int r, IList<string> results, string s)
+        {
+            if (l == 0 && r == 0)
+            {
+                results.Add(s);
+                return;
+            }
+
+            if (l > 0)
+            {
+                GenerateParenthesis(l - 1, r, results, s + "(");
+            }
+
+            if (r > 0 && r > l)
+            {
+                GenerateParenthesis(l, r - 1, results, s + ")");
+            }
+        }
+    }
+}
