a96cccc5b74e752bc43ef3e4234b00c14cb51a0a
BigEgg <cd_bigegg@hotmail.com>
2015-11-09 21:32:20 +0800

    #23 Add C# solution

---
+++ CSharp/LeetCode.Test/023-MergeKSortedLists-Test.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ListNode = LeetCode._023_MergeKSortedLists.ListNode;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _023_MergeKSortedLists_Test
+    {
+        [TestMethod]
+        public void MergeKListsTest()
+        {
+            var input = new ListNode[] {
+                GenerateList(new int[] { 1, 3, 5, 7, 9 }),
+                GenerateList(new int[] { 2, 4, 6, 8, 10 })
+            };
+
+            var solution = new _023_MergeKSortedLists();
+            var result = solution.MergeKLists(input);
+
+            AssertList(result, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+        }
+
+        [TestMethod]
+        public void MergeKListsTest_2()
+        {
+            var input = new ListNode[] {
+                GenerateList(new int[] { 1, 3, 7 }),
+                GenerateList(new int[] { 2, 10 }),
+                GenerateList(new int[] { 5, 9 }),
+                GenerateList(new int[] { 4, 6, 8})
+            };
+
+            var solution = new _023_MergeKSortedLists();
+            var result = solution.MergeKLists(input);
+
+            AssertList(result, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+        }
+
+        [TestMethod]
+        public void MergeKListsTest_OneList()
+        {
+            var input = new ListNode[] {
+                GenerateList(new int[] { 1, 2, 3 })
+            };
+
+            var solution = new _023_MergeKSortedLists();
+            var result = solution.MergeKLists(input);
+
+            AssertList(result, new int[] { 1, 2, 3 });
+        }
+
+        [TestMethod]
+        public void MergeKListsTest_EmptyList()
+        {
+            var input = new ListNode[] { };
+
+            var solution = new _023_MergeKSortedLists();
+            var result = solution.MergeKLists(input);
+
+            Assert.IsNull(result);
+        }
+
+
+        private ListNode GenerateList(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) { return null; }
+
+            var i = 0;
+            var first = new ListNode(nums[i]);
+            var current = first;
+
+            while (++i < nums.Length)
+            {
+                current.next = new ListNode(nums[i]);
+                current = current.next;
+            }
+
+            return first;
+        }
+
+        private void AssertList(ListNode first, int[] nums)
+        {
+            Assert.IsNotNull(first);
+            Assert.IsNotNull(nums);
+            Assert.IsTrue(nums.Length > 0);
+
+            var current = first;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Assert.IsNotNull(current);
+                Assert.AreEqual(nums[i], current.val);
+                current = current.next;
+            }
+
+            Assert.IsNull(current);
+        }
+    }
+}
---
+++ CSharp/LeetCode/023-MergeKSortedLists.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class _023_MergeKSortedLists
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            if (lists.Length == 0) { return null; }
+
+            var result = new List<ListNode>(lists);
+            ListNode head1, head2, newHead;
+            while (result.Count > 1)
+            {
+                head1 = result[0];
+                head2 = result[1];
+                newHead = MergeTwoLists(head1, head2);
+                result.RemoveRange(0, 2);
+                result.Add(newHead);
+            }
+
+            return result[0];
+        }
+
+
+        ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            var head = new ListNode(-1);
+            var current = head;
+
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val <= l2.val)
+                {
+                    current.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    current.next = l2;
+                    l2 = l2.next;
+                }
+                current = current.next;
+            }
+
+            current.next = l1 == null ? l2 : l1;
+            return head.next;
+        }
+
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+    }
+}
