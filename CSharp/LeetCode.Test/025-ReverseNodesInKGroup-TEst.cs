1227baad340aa4b7a824fc9626cb14637c83f6df
BigEgg <cd_bigegg@hotmail.com>
2015-11-11 23:51:25 +0800

    #25 Solved solution by C#

---
+++ CSharp/LeetCode.Test/025-ReverseNodesInKGroup-TEst.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ListNode = LeetCode._025_ReverseNodesInKGroup.ListNode;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _025_ReverseNodesInKGroup_Test
+    {
+        [TestMethod]
+        public void ReverseKGroupTest()
+        {
+            var input = GenerateList(new int[] { 1, 2, 3, 4, 5 });
+            var solution = new _025_ReverseNodesInKGroup();
+
+            var result = solution.ReverseKGroup(input, 2);
+            AssertList(result, new int[] { 2, 1, 4, 3, 5 });
+
+            input = GenerateList(new int[] { 1, 2, 3, 4, 5 });
+            result = solution.ReverseKGroup(input, 3);
+            AssertList(result, new int[] { 3, 2, 1, 4, 5 });
+        }
+
+        [TestMethod]
+        public void ReverseKGroupTest_EmptyList()
+        {
+            var solution = new _025_ReverseNodesInKGroup();
+            var result = solution.ReverseKGroup(null, 2);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void ReverseKGroupTest_KEqualZero()
+        {
+            var input = GenerateList(new int[] { 1, 2, 3, 4, 5 });
+
+            var solution = new _025_ReverseNodesInKGroup();
+            var result = solution.ReverseKGroup(input, 0);
+
+            AssertList(result, new int[] { 1, 2, 3, 4, 5 });
+        }
+
+        [TestMethod]
+        public void ReverseKGroupTest_KEqualOne()
+        {
+            var input = GenerateList(new int[] { 1, 2, 3, 4, 5 });
+
+            var solution = new _025_ReverseNodesInKGroup();
+            var result = solution.ReverseKGroup(input, 1);
+
+            AssertList(result, new int[] { 1, 2, 3, 4, 5 });
+        }
+
+        [TestMethod]
+        public void ReverseKGroupTest_KEqualLenght()
+        {
+            var input = GenerateList(new int[] { 1, 2, 3, 4, 5 });
+            var solution = new _025_ReverseNodesInKGroup();
+
+            var result = solution.ReverseKGroup(input, 5);
+            AssertList(result, new int[] { 5, 4, 3, 2, 1 });
+        }
+
+        [TestMethod]
+        public void ReverseKGroupTest_KLargerThanLenght()
+        {
+            var input = GenerateList(new int[] { 1, 2, 3, 4, 5 });
+            var solution = new _025_ReverseNodesInKGroup();
+
+            var result = solution.ReverseKGroup(input, 6);
+            AssertList(result, new int[] { 1, 2, 3, 4, 5 });
+        }
+
+        private ListNode GenerateList(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) { return null; }
+
+            var i = 0;
+            var first = new ListNode(nums[i]);
+            var current = first;
+
+            while (++i < nums.Length)
+            {
+                current.next = new ListNode(nums[i]);
+                current = current.next;
+            }
+
+            return first;
+        }
+
+        private void AssertList(ListNode first, int[] nums)
+        {
+            Assert.IsNotNull(first);
+            Assert.IsNotNull(nums);
+            Assert.IsTrue(nums.Length > 0);
+
+            var current = first;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Assert.IsNotNull(current);
+                Assert.AreEqual(nums[i], current.val);
+                current = current.next;
+            }
+
+            Assert.IsNull(current);
+        }
+    }
+}
---
+++ CSharp/LeetCode/025-ReverseNodesInKGroup.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode
+{
+    public class _025_ReverseNodesInKGroup
+    {
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            if (k <= 1) { return head; }
+
+            var dummyHead = new ListNode(-1);
+            dummyHead.next = head;
+            ListNode p = dummyHead, q, r;
+            var notEnough = false;
+            int i = 0;
+
+            while (p.next != null)
+            {
+                q = p.next;
+                for (i = 0; i < k; i++)
+                {
+                    if (q == null) { notEnough = true; break; }
+                    q = q.next;
+                }
+                if (notEnough) { break; }
+
+                q = p.next;
+                for (i = 1; i < k; i++)
+                {
+                    r = q.next;
+                    q.next = r.next;
+                    r.next = p.next;
+                    p.next = r;
+                }
+
+                p = q;
+            }
+
+            return dummyHead.next;
+        }
+
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+    }
+}
