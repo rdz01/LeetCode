ca1948e73fde68ff375a68dd12891a800ae61633
BigEgg <cd_bigegg@hotmail.com>
2015-11-13 23:36:22 +0800

    #26 Add C# solution

---
+++ CSharp/LeetCode.Test/026-RemoveDuplicatesFromSortedArray-Test.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _026_RemoveDuplicatesFromSortedArray_Test
+    {
+        [TestMethod]
+        public void RemoveDuplicatesTest()
+        {
+            var input = new int[] { 1, 1, 2 };
+
+            var solution = new _026_RemoveDuplicatesFromSortedArray();
+            var result = solution.RemoveDuplicates(input);
+
+            Assert.AreEqual(2, result);
+            AssertArray(new int[] { 1, 2 }, input);
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesTest_EmptyInput()
+        {
+            var solution = new _026_RemoveDuplicatesFromSortedArray();
+            var result = solution.RemoveDuplicates(new int[] { });
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesTest_OneItem()
+        {
+            var input = new int[] { 1 };
+
+            var solution = new _026_RemoveDuplicatesFromSortedArray();
+            var result = solution.RemoveDuplicates(input);
+
+            Assert.AreEqual(1, result);
+            AssertArray(new int[] { 1 }, input);
+        }
+
+        private void AssertArray(int[] expected, int[] actual)
+        {
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
---
+++ CSharp/LeetCode/026-RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode
+{
+    public class _026_RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length <= 1) { return nums.Length; }
+
+            var index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[index]) { continue; }
+
+                nums[++index] = nums[i];
+            }
+
+            return index + 1;
+        }
+    }
+}
