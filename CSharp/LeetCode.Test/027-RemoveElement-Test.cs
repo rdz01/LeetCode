e530b2ec4eb5ae6e600d55b1a6b195ba794d848f
BigEgg <cd_bigegg@hotmail.com>
2015-11-14 22:50:26 +0800

    #27 Add C# solution

---
+++ CSharp/LeetCode.Test/027-RemoveElement-Test.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _027_RemoveElement_Test
+    {
+        [TestMethod]
+        public void RemoveElementTest()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5 };
+
+            var solution = new _027_RemoveElement();
+            var result = solution.RemoveElement(input, 3);
+
+            Assert.AreEqual(4, result);
+            AssertArray(new int[] { 1, 2, 5, 4 }, input);
+        }
+
+        [TestMethod]
+        public void RemoveElementTest_EmptyInput()
+        {
+            var solution = new _027_RemoveElement();
+            var result = solution.RemoveElement(new int[] { }, 3);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void RemoveElementTest_OnlyOneItem_NotSame()
+        {
+            var input = new int[] { 1 };
+
+            var solution = new _027_RemoveElement();
+            var result = solution.RemoveElement(input, 3);
+
+            Assert.AreEqual(1, result);
+            AssertArray(new int[] { 1 }, input);
+        }
+
+        [TestMethod]
+        public void RemoveElementTest_OnlyOneItem_Same()
+        {
+            var input = new int[] { 1 };
+
+            var solution = new _027_RemoveElement();
+            var result = solution.RemoveElement(input, 1);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void RemoveElementTest_HaveMultipleValue()
+        {
+            var input = new int[] { 1, 2, 3, 3, 4, 5 };
+
+            var solution = new _027_RemoveElement();
+            var result = solution.RemoveElement(input, 3);
+
+            Assert.AreEqual(4, result);
+            AssertArray(new int[] { 1, 2, 5, 4 }, input);
+        }
+
+        [TestMethod]
+        public void RemoveElementTest_AllValue()
+        {
+            var input = new int[] { 3, 3, 3, 3, 3, 3 };
+
+            var solution = new _027_RemoveElement();
+            var result = solution.RemoveElement(input, 3);
+
+            Assert.AreEqual(0, result);
+        }
+
+
+        private void AssertArray(int[] expected, int[] actual)
+        {
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
---
+++ CSharp/LeetCode/027-RemoveElement.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode
+{
+    public class _027_RemoveElement
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            var index = 0;
+            var lastIndex = nums.Length - 1;
+            var temp = 0;
+
+            while (index <= lastIndex)
+            {
+                if (nums[index] == val)
+                {
+                    temp = nums[lastIndex];
+                    nums[lastIndex] = val;
+                    nums[index] = temp;
+                    lastIndex--;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+            return lastIndex + 1;
+        }
+    }
+}
