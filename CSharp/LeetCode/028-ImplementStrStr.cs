7e649aab5872e094095688bb83982bdf3d4ef894
BigEgg <cd_bigegg@hotmail.com>
2015-11-15 13:58:10 +0800

    #28 Use KMP algorithm

--- CSharp/LeetCode/028-ImplementStrStr.cs
+++ CSharp/LeetCode/028-ImplementStrStr.cs
@@ -8,21 +8,60 @@
             if (needleLength == 0) { return 0; }
 
             var haystackLength = haystack.Length;
-            var needleIndex = 0;
-            for (int i = 0; i < haystackLength; i++)
+            int i, j;
+            if (needleLength == 1)
             {
-                needleIndex = 0;
-                while (needleIndex < needleLength
-                    && i + needleIndex < haystackLength
-                    && haystack[i + needleIndex] == needle[needleIndex])
+                for (i = 0; i < haystackLength; i++)
                 {
-                    needleIndex++;
+                    if (haystack[i] == needle[0]) { return i; }
                 }
-                if (needleIndex == needleLength)
+
+                return -1;
+            }
+
+            var partialMatchTable = new int[needleLength];
+            partialMatchTable[0] = -1;
+            partialMatchTable[1] = 0;
+            i = 2; j = 0;
+            while (i < needleLength)
+            {
+                if (needle[i - 1] == needle[j])
                 {
-                    return i;
+                    partialMatchTable[i++] = ++j;
+                }
+                else if (j > 0)
+                {
+                    j = partialMatchTable[j];
+                }
+                else
+                {
+                    partialMatchTable[i++] = 0;
                 }
             }
+
+            i = 0; j = 0;
+            while (i + j < haystackLength)
+            {
+                if (needle[j] == haystack[i + j])
+                {
+                    if (j == needleLength - 1) { return i; }
+                    j++;
+                }
+                else
+                {
+                    if (partialMatchTable[j] > -1)
+                    {
+                        i = i + j - partialMatchTable[j];
+                        j = partialMatchTable[j];
+                    }
+                    else
+                    {
+                        j = 0;
+                        i++;
+                    }
+                }
+            }
+
             return -1;
         }
     }
