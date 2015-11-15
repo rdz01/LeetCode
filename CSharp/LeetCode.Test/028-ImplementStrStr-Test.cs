3ce143030cab867a43f7f88c43d2f9d3e91390a7
BigEgg <cd_bigegg@hotmail.com>
2015-11-15 13:17:39 +0800

    #28 Use brute force to solve the problem with C#

---
+++ CSharp/LeetCode.Test/028-ImplementStrStr-Test.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _028_ImplementStrStr_Test
+    {
+        [TestMethod]
+        public void StrStrTest()
+        {
+            var solution = new _028_ImplementStrStr();
+            var result = solution.StrStr("abcdefg", "b");
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void StrStrTest_EmptyNeedle()
+        {
+            var solution = new _028_ImplementStrStr();
+            var result = solution.StrStr("abcdefg", "");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void StrStrTest_NeedleLongerThanRest()
+        {
+            var solution = new _028_ImplementStrStr();
+            var result = solution.StrStr("abcdefg", "gh");
+
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
---
+++ CSharp/LeetCode/028-ImplementStrStr.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode
+{
+    public class _028_ImplementStrStr
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            var needleLength = needle.Length;
+            if (needleLength == 0) { return 0; }
+
+            var haystackLength = haystack.Length;
+            var needleIndex = 0;
+            for (int i = 0; i < haystackLength; i++)
+            {
+                needleIndex = 0;
+                while (needleIndex < needleLength
+                    && i + needleIndex < haystackLength
+                    && haystack[i + needleIndex] == needle[needleIndex])
+                {
+                    needleIndex++;
+                }
+                if (needleIndex == needleLength)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
