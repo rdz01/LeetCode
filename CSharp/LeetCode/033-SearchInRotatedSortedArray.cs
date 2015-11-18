4de08cfa44b4758a6f8866e220ac5ab79e61d8f9
BigEgg <cd_bigegg@hotmail.com>
2015-11-18 23:51:17 +0800

    #33 Add C# solution

---
+++ CSharp/LeetCode/033-SearchInRotatedSortedArray.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode
+{
+    public class _033_SearchInRotatedSortedArray
+    {
+        public int Search(int[] nums, int target)
+        {
+            int lo = 0, hi = nums.Length - 1;
+            int mid;
+
+            while (lo <= hi)
+            {
+                if (nums[lo] <= nums[hi] && (target < nums[lo] || target > nums[hi]))
+                {
+                    return -1;
+                }
+
+                mid = lo + (hi - lo) / 2;
+                if (target == nums[mid]) { return mid; }
+
+                if (nums[lo] <= nums[mid])
+                {
+                    if (nums[lo] <= target && target < nums[mid])
+                    {
+                        hi = mid - 1;
+                    }
+                    else
+                    {
+                        lo = mid + 1;
+                    }
+                }
+                else
+                {
+                    if (target <= nums[hi] && nums[mid] < target)
+                    {
+                        lo = mid + 1;
+                    }
+                    else
+                    {
+                        hi = mid - 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
