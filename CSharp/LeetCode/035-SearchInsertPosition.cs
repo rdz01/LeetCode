4d89f7c3943b6f14ff7e332bc02d110d298ec4a4
BigEgg <cd_bigegg@hotmail.com>
2015-11-19 20:34:23 +0800

    #35 Add C# solution

---
+++ CSharp/LeetCode/035-SearchInsertPosition.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    public class _035_SearchInsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int lo = 0, hi = nums.Length, mid = 0;
+
+            while (lo < hi)
+            {
+                mid = lo + (hi - lo) / 2;
+
+                if (target == nums[mid]) { return mid; }
+                else if (target > nums[mid]) { lo = mid + 1; }
+                else { hi = mid; }
+            }
+
+            return lo;
+        }
+    }
+}
