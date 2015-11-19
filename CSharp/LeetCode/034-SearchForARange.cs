5fa301a05cee04814bfd79bc588c0505d2524ab2
BigEgg <cd_bigegg@hotmail.com>
2015-11-19 14:23:38 +0800

    #34 Add C# solution

---
+++ CSharp/LeetCode/034-SearchForARange.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode
+{
+    public class _034_SearchForARange
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            int lo = 0, hi = nums.Length, mid = 0;
+            while (lo < hi)
+            {
+                mid = lo + (hi - lo) / 2;
+
+                if (target > nums[mid]) { lo = mid + 1; }
+                else { hi = mid; }
+            }
+            if (lo == nums.Length || nums[lo] != target)
+            {
+                return new int[] { -1, -1 };
+            }
+
+            var result = new int[2];
+            result[0] = lo;
+
+            lo = mid; hi = nums.Length; mid = 0;
+            while (lo < hi)
+            {
+                mid = lo + (hi - lo) / 2;
+
+                if (target >= nums[mid]) { lo = mid + 1; }
+                else { hi = mid; }
+            }
+            result[1] = lo - 1;
+
+            return result;
+        }
+    }
+}
