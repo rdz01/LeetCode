1ec8d377a965afa1f85b6907444509a248c271ac
BigEgg <cd_bigegg@hotmail.com>
2015-11-21 22:08:32 +0800

    #39 Add C# solution

---
+++ CSharp/LeetCode.Test/039-CombinationSum-Test.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _039_CombinationSum_Test
+    {
+        [TestMethod]
+        public void CombinationSumTest_WhenCandidiatesOrdered()
+        {
+            var solution = new _039_CombinationSum();
+            var result = solution.CombinationSum(new int[] { 2, 3, 6, 7 }, 7);
+
+            AssertList(new List<IList<int>>()
+            {
+                new List<int>() { 2, 2, 3 },
+                new List<int>() { 7 }
+            }, result);
+        }
+
+        [TestMethod]
+        public void CombinationSumTest_WhenCandidiatesWithoutOrdered()
+        {
+            var solution = new _039_CombinationSum();
+            var result = solution.CombinationSum(new int[] { 3, 6, 2, 7 }, 7);
+
+            AssertList(new List<IList<int>>()
+            {
+                new List<int>() { 2, 2, 3 },
+                new List<int>() { 7 }
+            }, result);
+        }
+
+        [TestMethod]
+        public void CombinationSumTest_NoMatch()
+        {
+            var solution = new _039_CombinationSum();
+            var result = solution.CombinationSum(new int[] { 2, 3, 6, 7 }, 1);
+
+            AssertList(new List<IList<int>>() { }, result);
+        }
+
+
+        void AssertList(IList<IList<int>> expected, IList<IList<int>> actual)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i].Count, actual[i].Count);
+                for (int j = 0; j < expected[i].Count; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+        }
+    }
+}
---
+++ CSharp/LeetCode/039-CombinationSum.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class _039_CombinationSum
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            ThreeWayQuickSort(candidates, 0, candidates.Length - 1);
+
+            var result = new List<IList<int>>();
+            DeepFirstSearch(candidates, target, 0, new List<int>(), result);
+            return result;
+        }
+
+        void DeepFirstSearch(int[] candidates, int gap, int startIndex, IList<int> tempResult, IList<IList<int>> result)
+        {
+            for (int i = startIndex; i < candidates.Length; i++)
+            {
+                if (candidates[i] > gap) { return; }
+
+                tempResult.Add(candidates[i]);
+
+                if (candidates[i] == gap)
+                {
+                    result.Add(new List<int>(tempResult));
+                }
+                else
+                {
+                    DeepFirstSearch(candidates, gap - candidates[i], i, tempResult, result);
+                }
+                tempResult.RemoveAt(tempResult.Count - 1);
+            }
+        }
+
+        void ThreeWayQuickSort(int[] nums, int lo, int hi)
+        {
+            if (hi <= lo) { return; }
+            int i = lo, lt = lo, gt = hi;
+            int v = nums[lo];
+            int temp;
+            while (i <= gt)
+            {
+                if (nums[i] < v)
+                {
+                    temp = nums[i];
+                    nums[i] = nums[lt];
+                    nums[lt] = temp;
+                    i++;
+                    lt++;
+                }
+                else if (nums[i] > v)
+                {
+                    temp = nums[i];
+                    nums[i] = nums[gt];
+                    nums[gt] = temp;
+                    gt--;
+                }
+                else { i++; }
+            }
+            ThreeWayQuickSort(nums, lo, lt - 1);
+            ThreeWayQuickSort(nums, gt + 1, hi);
+        }
+    }
+}
