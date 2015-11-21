9c344938409d244395edb7e72a752981de00ad4c
BigEgg <cd_bigegg@hotmail.com>
2015-11-21 22:09:38 +0800

    #39 Use System sort algorithm

--- CSharp/LeetCode/039-CombinationSum.cs
+++ CSharp/LeetCode/039-CombinationSum.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace LeetCode
 {
@@ -6,7 +7,7 @@ namespace LeetCode
     {
         public IList<IList<int>> CombinationSum(int[] candidates, int target)
         {
-            ThreeWayQuickSort(candidates, 0, candidates.Length - 1);
+            Array.Sort(candidates);
 
             var result = new List<IList<int>>();
             DeepFirstSearch(candidates, target, 0, new List<int>(), result);
@@ -31,35 +32,6 @@ namespace LeetCode
                 }
                 tempResult.RemoveAt(tempResult.Count - 1);
             }
-        }
-
-        void ThreeWayQuickSort(int[] nums, int lo, int hi)
-        {
-            if (hi <= lo) { return; }
-            int i = lo, lt = lo, gt = hi;
-            int v = nums[lo];
-            int temp;
-            while (i <= gt)
-            {
-                if (nums[i] < v)
-                {
-                    temp = nums[i];
-                    nums[i] = nums[lt];
-                    nums[lt] = temp;
-                    i++;
-                    lt++;
-                }
-                else if (nums[i] > v)
-                {
-                    temp = nums[i];
-                    nums[i] = nums[gt];
-                    nums[gt] = temp;
-                    gt--;
-                }
-                else { i++; }
-            }
-            ThreeWayQuickSort(nums, lo, lt - 1);
-            ThreeWayQuickSort(nums, gt + 1, hi);
         }
     }
 }
