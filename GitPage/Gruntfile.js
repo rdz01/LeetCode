7c823b24f3cdab205bf1cc377b421aa4b3b6afa0
BigEgg <cd_bigegg@hotmail.com>
2015-11-22 20:13:33 +0800

    Remove angular route library since angular-material already integrate it

--- GitPage/Gruntfile.js
+++ GitPage/Gruntfile.js
@@ -8,19 +8,31 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         clean: {
-            build: {
-                src: [SRC_DIR, '!' + SRC_DIR + 'scripts/com/**/*.js', SRC_DIR + 'scripts/main.js']
+            prebuild: {
+                src: [SRC_DIR + 'scripts/vendor/', SRC_DIR + 'styles/vendor/']
             }
         },
         copy: {
-            vendor: {
+            vendorjs: {
                 cwd: NODE_MODULES_DIR,
                 src: [
-                    'angular/*.min.js',
-                    'angular-route/angular-route.min.js'
+                    'angular/angular.min.js',
+                    'angular-animate/angular-animate.min.js',
+                    'angular-aria/angular-aria.min.js',
+                    'angular-material/angular-material.min.js'
                 ],
                 dest: SRC_DIR + 'scripts/vendor/',
-                expand: true
+                expand: true,
+                flatten: true
+            },
+            vendorcss: {
+                cwd: NODE_MODULES_DIR,
+                src: [
+                    'angular-material/angular-material.min.css'
+                ],
+                dest: SRC_DIR + 'styles/vendor/',
+                expand: true,
+                flatten: true
             },
             publish: {
                 cwd: SRC_DIR,
@@ -47,8 +59,7 @@ module.exports = function (grunt) {
                 'Gruntfile.js',
                 SRC_DIR + 'scripts/com/**/*.js',
                 SRC_DIR + 'scripts/e2e/**/*.js',
-                SRC_DIR + 'scripts/spec/**/*.js',
-                'mock_server/**/*.js'
+                SRC_DIR + 'scripts/spec/**/*.js'
             ],
             options: {
                 reporter: require('jshint-stylish'),
@@ -95,16 +106,17 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    // grunt.loadNpmTasks('grunt-contrib-jshint');
+    // grunt.loadNpmTasks('grunt-contrib-less');
+    // grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
-    grunt.loadNpmTasks('grunt-protractor-runner');
+    // grunt.loadNpmTasks('grunt-contrib-jasmine');
+    // grunt.loadNpmTasks('grunt-protractor-runner');
 
-    grunt.registerTask('default', ['jshint', 'jasmine', 'clean', 'less', 'concat', 'copy']);
-    grunt.registerTask('compile', ['jshint', 'jasmine', 'clean:build', 'less', 'concat', 'protractor', 'copy:vendor']);
-    grunt.registerTask('unit-test', ['jshint', 'jasmine']);
-    grunt.registerTask('e2e-test', ['jshint', 'jasmine', 'clean', 'less', 'concat', 'protractor']);
+    // grunt.registerTask('default', ['jshint', 'jasmine', 'clean', 'less', 'concat', 'copy']);
+    grunt.registerTask('pre-build', ['clean:prebuild', 'copy:vendorjs', 'copy:vendorcss']);
+    // grunt.registerTask('build', ['jshint', 'jasmine', 'less', 'concat', 'protractor', ]);
+    // grunt.registerTask('unit-test', ['jshint', 'jasmine']);
+    // grunt.registerTask('e2e-test', ['jshint', 'jasmine', 'clean', 'less', 'concat', 'protractor']);
 };
