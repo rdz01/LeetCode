7f6b57e9754146599593060f1faddfc252117a36
BigEgg <cd_bigegg@hotmail.com>
2015-11-24 22:33:23 +0800

    #42 Add C# solution

---
+++ CSharp/LeetCode/042-TrappingRainWater.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode
+{
+    public class _042_TrappingRainWater
+    {
+        public int Trap(int[] height)
+        {
+            int n = height.Length;
+            int[] maxLeft = new int[n],
+                  maxRight = new int[n];
+
+            for (int i = 1; i < n; i++)
+            {
+                maxLeft[i] = maxLeft[i - 1] > height[i - 1] ? maxLeft[i - 1] : height[i - 1];
+                maxRight[n - i - 1] = maxRight[n - i] > height[n - i] ? maxRight[n - i] : height[n - i];
+            }
+
+            int minHeight, result = 0;
+            for (int i = 0; i < n; i++)
+            {
+                minHeight = maxLeft[i] < maxRight[i] ? maxLeft[i] : maxRight[i];
+                if (minHeight > height[i])
+                {
+                    result += minHeight - height[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
