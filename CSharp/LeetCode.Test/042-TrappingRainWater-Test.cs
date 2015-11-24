27a6d31065ce3f05d5d62da614af0c0111ad9b09
BigEgg <cd_bigegg@hotmail.com>
2015-11-24 22:49:36 +0800

    #42 reduce space usage

--- CSharp/LeetCode.Test/042-TrappingRainWater-Test.cs
+++ CSharp/LeetCode.Test/042-TrappingRainWater-Test.cs
@@ -17,6 +17,17 @@ namespace LeetCode.Test
         }
 
         [TestMethod]
+        public void TrapTest_Balance()
+        {
+            var input = new int[] { 2, 0, 2 };
+
+            var solution = new _042_TrappingRainWater();
+            var result = solution.Trap(input);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
         public void TrapTest_FirstOneHigher()
         {
             var input = new int[] { 2, 1, 0, 1, 0, 1, 0, 1, 0, 1 };
@@ -56,6 +67,15 @@ namespace LeetCode.Test
 
             var solution = new _042_TrappingRainWater();
             var result = solution.Trap(input);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TrapTest_EmptyInput()
+        {
+            var solution = new _042_TrappingRainWater();
+            var result = solution.Trap(new int[] { });
 
             Assert.AreEqual(0, result);
         }
--- CSharp/LeetCode/042-TrappingRainWater.cs
+++ CSharp/LeetCode/042-TrappingRainWater.cs
@@ -5,22 +5,33 @@
         public int Trap(int[] height)
         {
             int n = height.Length;
-            int[] maxLeft = new int[n],
-                  maxRight = new int[n];
+            if (n == 0) { return 0; }
+
+            int maxLeftHeight = height[0], maxRightHeight = height[n - 1];
+            int result = 0, tempResultLeft = 0, tempResultRight = 0; ;
 
             for (int i = 1; i < n; i++)
             {
-                maxLeft[i] = maxLeft[i - 1] > height[i - 1] ? maxLeft[i - 1] : height[i - 1];
-                maxRight[n - i - 1] = maxRight[n - i] > height[n - i] ? maxRight[n - i] : height[n - i];
-            }
-
-            int minHeight, result = 0;
-            for (int i = 0; i < n; i++)
-            {
-                minHeight = maxLeft[i] < maxRight[i] ? maxLeft[i] : maxRight[i];
-                if (minHeight > height[i])
+                if (maxLeftHeight > height[i])
                 {
-                    result += minHeight - height[i];
+                    tempResultLeft += maxLeftHeight - height[i];
+                }
+                else
+                {
+                    maxLeftHeight = height[i];
+                    result += tempResultLeft;
+                    tempResultLeft = 0;
+                }
+
+                if (maxRightHeight > height[n - i - 1])
+                {
+                    tempResultRight += maxRightHeight - height[n - i - 1];
+                }
+                else if (maxRightHeight < height[n - i - 1])
+                {
+                    maxRightHeight = height[n - i - 1];
+                    result += tempResultRight;
+                    tempResultRight = 0;
                 }
             }
 
