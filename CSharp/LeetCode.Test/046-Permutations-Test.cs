d011539f607aef16f43855ef43478f8a52072a39
BigEgg <cd_bigegg@hotmail.com>
2015-11-27 00:20:44 +0800

    #46 Add C# solution

---
+++ CSharp/LeetCode.Test/046-Permutations-Test.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _046_Permutations_Test
+    {
+        [TestMethod]
+        public void PermuteTest()
+        {
+            var input = new int[] { 1, 2, 3 };
+
+            var solution = new _046_Permutations();
+            var result = solution.Permute(input);
+
+            Assert.AreEqual(6, result.Count);
+            AssertList(new List<IList<int>>()
+            {
+                new List<int> { 1, 2, 3},
+                new List<int> { 2, 1, 3},
+                new List<int> { 3, 1, 2},
+                new List<int> { 1, 3, 2},
+                new List<int> { 2, 3, 1},
+                new List<int> { 3, 2, 1}
+            }, result);
+        }
+
+        [TestMethod]
+        public void PermuteTest_2()
+        {
+            var input = new int[] { 1, 2, 3, 4 };
+
+            var solution = new _046_Permutations();
+            var result = solution.Permute(input);
+
+            Assert.AreEqual(24, result.Count);
+        }
+
+        [TestMethod]
+        public void PermuteTest_OneItem()
+        {
+            var input = new int[] { 1 };
+
+            var solution = new _046_Permutations();
+            var result = solution.Permute(input);
+
+            Assert.AreEqual(1, result.Count);
+        }
+
+
+        void AssertList(IList<IList<int>> expected, IList<IList<int>> actual)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i].Count, actual[i].Count);
+                for (int j = 0; j < expected[i].Count; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+        }
+    }
+}
---
+++ CSharp/LeetCode/046-Permutations.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class _046_Permutations
+    {
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            result.Add(new List<int>(nums));
+
+            int length = nums.Length;
+            int size, temp, i, j, k;
+            IList<int> tempList;
+            for (i = 0; i < length; i++)
+            {
+                size = result.Count;
+                for (j = 0; j < size; j++)
+                {
+                    tempList = new List<int>(result[j]);
+                    for (k = i + 1; k < length; k++)
+                    {
+                        temp = tempList[k];
+                        tempList[k] = tempList[i];
+                        tempList[i] = temp;
+                        result.Add(new List<int>(tempList));
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
