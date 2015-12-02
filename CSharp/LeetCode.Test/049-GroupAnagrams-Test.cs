09c72b64e50f6d8e432792fcec45d0ef791e450f
BigEgg <cd_bigegg@hotmail.com>
2015-12-02 23:26:15 +0800

    #49 Add C# solution

---
+++ CSharp/LeetCode.Test/049-GroupAnagrams-Test.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _049_GroupAnagrams_Test
+    {
+        [TestMethod]
+        public void GroupAnagramsTest()
+        {
+            var input = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+            var solution = new _049_GroupAnagrams();
+            var result = solution.GroupAnagrams(input);
+
+            AssertList(
+                new List<IList<string>>()
+                {
+                    new List<string> () { "ate", "eat", "tea" },
+                    new List<string> () { "nat","tan" },
+                    new List<string> () { "bat" }
+                }, result);
+        }
+
+        [TestMethod]
+        public void GroupAnagramsTest_Empty()
+        {
+            var input = new string[] { };
+
+            var solution = new _049_GroupAnagrams();
+            var result = solution.GroupAnagrams(input);
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        void AssertList(IList<IList<string>> expected, IList<IList<string>> actual)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i].Count, actual[i].Count);
+                for (int j = 0; j < expected[i].Count; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+        }
+    }
+}
---
+++ CSharp/LeetCode/049-GroupAnagrams.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class _049_GroupAnagrams
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var result = new List<IList<string>>();
+            var mapping = new Dictionary<string, int>();
+
+            var ch = string.Empty;
+            foreach (var str in strs)
+            {
+                ch = new string(str.OrderBy(c => c).ToArray());
+                if (!mapping.ContainsKey(ch))
+                {
+                    result.Add(new List<string>());
+                    mapping[ch] = result.Count - 1;
+                }
+                result[mapping[ch]].Add(str);
+            }
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                result[i] = result[i].OrderBy(s => s).ToList();
+            }
+
+            return result;
+        }
+    }
+}
