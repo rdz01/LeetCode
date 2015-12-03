f76fad4c87420513a8e893e935f8a03802488cad
BigEgg <cd_bigegg@hotmail.com>
2015-12-03 23:37:53 +0800

    #50 Add C# solution

---
+++ CSharp/LeetCode/050-Pow.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode
+{
+    public class _050_Pow
+    {
+        public double MyPow(double x, int n)
+        {
+            if (x == 0) { return 0; }
+
+            var sign = false;
+            if (n < 0)
+            {
+                n = -n;
+                sign = true;
+            }
+
+            double result = 1;
+            while (n > 0)
+            {
+                if ((n & 1) == 1)
+                {
+                    result *= x;
+                }
+                n = n >> 1;
+                x = x * x;
+            }
+
+            return sign ? 1 / result : result;
+        }
+    }
+}
