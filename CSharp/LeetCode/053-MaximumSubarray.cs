1ab68cd74fa7bb2c411c5aaa45d639f042a48873
BigEgg <cd_bigegg@hotmail.com>
2015-12-06 17:26:06 +0800

    #53 Use brute force to solve the problem

---
+++ CSharp/LeetCode/053-MaximumSubarray.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode
+{
+    public class _053_MaximumSubarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int temp, result = int.MinValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                temp = 0;
+                for (int j = i; j < nums.Length; j++)
+                {
+                    temp += nums[j];
+                    if (result < temp)
+                    {
+                        result = temp;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- Problems/053-MaximumSubarray.md
+++ Problems/053-MaximumSubarray.md
@@ -4,9 +4,9 @@ Find the contiguous subarray within an array (containing at least one number) wh
 
 For example,
 
-> given the array `[−2,1,−3,4,−1,2,1,−5,4]`,
+> given the array `[-2,1,-3,4,-1,2,1,-5,4]`,
 > 
-> the contiguous subarray `[4,−1,2,1]` has the largest sum = `6`.
+> the contiguous subarray `[4,-1,2,1]` has the largest sum = `6`.
 
 **More practice:**
 
