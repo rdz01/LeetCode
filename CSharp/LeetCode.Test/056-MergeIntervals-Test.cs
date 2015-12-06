7714c506dcd000e35267882db2b3287043fac99e
BigEgg <cd_bigegg@hotmail.com>
2015-12-06 19:53:49 +0800

    #56 Add C# solution

---
+++ CSharp/LeetCode.Test/056-MergeIntervals-Test.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _056_MergeIntervals_Test
+    {
+        [TestMethod]
+        public void MergeTest()
+        {
+            var input = new[]
+            {
+                new _056_MergeIntervals.Interval(1, 3),
+                new _056_MergeIntervals.Interval(2, 6),
+                new _056_MergeIntervals.Interval(8, 10),
+                new _056_MergeIntervals.Interval(15, 18),
+            };
+
+            var solution = new _056_MergeIntervals();
+            var result = solution.Merge(input);
+
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(1, result[0].start);
+            Assert.AreEqual(6, result[0].end);
+            Assert.AreEqual(8, result[1].start);
+            Assert.AreEqual(10, result[1].end);
+            Assert.AreEqual(15, result[2].start);
+            Assert.AreEqual(18, result[2].end);
+        }
+
+        [TestMethod]
+        public void MergeTest_NotOrdered()
+        {
+            var input = new[]
+            {
+                new _056_MergeIntervals.Interval(1, 3),
+                new _056_MergeIntervals.Interval(0, 2)
+            };
+
+            var solution = new _056_MergeIntervals();
+            var result = solution.Merge(input);
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(0, result[0].start);
+            Assert.AreEqual(3, result[0].end);
+        }
+
+        [TestMethod]
+        public void MergeTest_Empty()
+        {
+            var input = new List<_056_MergeIntervals.Interval>();
+
+            var solution = new _056_MergeIntervals();
+            var result = solution.Merge(input);
+
+            Assert.AreEqual(0, result.Count);
+        }
+    }
+}
---
+++ CSharp/LeetCode/056-MergeIntervals.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class _056_MergeIntervals
+    {
+        public IList<Interval> Merge(IList<Interval> intervals)
+        {
+            var comparer = new IntervalComparer();
+            var list = intervals.ToList();
+            list.Sort(comparer);
+
+            var result = new List<Interval>();
+            Interval lastInterval;
+            foreach (var interval in list)
+            {
+                lastInterval = result.LastOrDefault();
+                if (lastInterval != null && lastInterval.end >= interval.start)
+                {
+
+                    if (lastInterval.end < interval.end)
+                        lastInterval.end = interval.end;
+                }
+                else
+                {
+                    result.Add(interval);
+                }
+            }
+
+            return result;
+        }
+
+        public class IntervalComparer : IComparer<Interval>
+        {
+            public int Compare(Interval x, Interval y)
+            {
+                if (x.start == y.start) return x.end - y.end;
+                else return x.start - y.start;
+            }
+        }
+
+        public class Interval
+        {
+            public int start;
+            public int end;
+            public Interval() { start = 0; end = 0; }
+            public Interval(int s, int e) { start = s; end = e; }
+        }
+    }
+}
