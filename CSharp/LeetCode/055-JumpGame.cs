898bbb82ce5dcdde4690abb1cf1a3dba6c5326fd
BigEgg <cd_bigegg@hotmail.com>
2015-12-06 19:18:49 +0800

    #55 Add C# solution

---
+++ CSharp/LeetCode/055-JumpGame.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    public class _055_JumpGame
+    {
+        public bool CanJump(int[] nums)
+        {
+            int maxRight = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > maxRight) { return false; }
+                if (i + nums[i] > maxRight)
+                    maxRight = i + nums[i];
+                if (maxRight >= nums.Length - 1)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
