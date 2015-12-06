f7f7fc48353ced02e20324a1a99126c8075c47e4
BigEgg <cd_bigegg@hotmail.com>
2015-12-06 20:28:40 +0800

    #57 Add C# solution

---
+++ CSharp/LeetCode/057-InsertInterval.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class _057_InsertInterval
+    {
+        public IList<Interval> Insert(IList<Interval> intervals, Interval newInterval)
+        {
+            var result = new List<Interval>();
+
+            for (int i = 0; i < intervals.Count; i++)
+            {
+                if (newInterval.start > intervals[i].end)
+                {
+                    result.Add(intervals[i]);
+                }
+                else if (newInterval.end < intervals[i].start)
+                {
+                    result.Add(newInterval);
+                    for (int j = i; j < intervals.Count; j++)
+                    {
+                        result.Add(intervals[j]);
+                    }
+                    return result;
+                }
+                else
+                {
+                    newInterval.start = Math.Min(intervals[i].start, newInterval.start);
+                    newInterval.end = Math.Max(intervals[i].end, newInterval.end);
+                }
+            }
+
+            result.Add(newInterval);
+            return result;
+        }
+
+        public class Interval
+        {
+            public int start;
+            public int end;
+            public Interval() { start = 0; end = 0; }
+            public Interval(int s, int e) { start = s; end = e; }
+        }
+    }
+}
