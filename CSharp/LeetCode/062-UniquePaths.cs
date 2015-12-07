f7204e6eaa309c05abe25b8fe159f84ca51e7efa
BigEgg <cd_bigegg@hotmail.com>
2015-12-07 21:01:39 +0800

    #62 Add C# solution

---
+++ CSharp/LeetCode/062-UniquePaths.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class _062_UniquePaths
+    {
+        public int UniquePaths(int m, int n)
+        {
+            var rest = Math.Min(m - 1, n - 1);
+            if (rest == 0) { return 1; }
+            if (rest == 1) { return m + n - 2; }
+
+            long result = 1;
+            int temp = m + n - 2, i = rest;
+            while (i-- > 0)
+            {
+                result *= temp--;
+            }
+
+            while (rest > 1)
+            {
+                result /= rest--;
+            }
+
+            return (int)result;
+        }
+    }
+}
