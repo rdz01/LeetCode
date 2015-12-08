1ba1a791739a05653c6504be5bfff1622b582df0
BigEgg <cd_bigegg@hotmail.com>
2015-12-08 21:13:38 +0800

    #63 Add C# solution

---
+++ CSharp/LeetCode.Test/063-UniquePaths2-Test.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _063_UniquePaths2_Test
+    {
+        [TestMethod]
+        public void UniquePathsWithObstaclesTest()
+        {
+            var input = new int[3, 3]
+            {
+                { 0, 0, 0 },
+                { 0, 1, 0 },
+                { 0, 0, 0 }
+            };
+
+            var solution = new _063_UniquePaths2();
+            var result = solution.UniquePathsWithObstacles(input);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void UniquePathsWithObstaclesTest_ObstacleOnFirstRow()
+        {
+            var input = new int[3, 3]
+            {
+                { 0, 1, 0 },
+                { 0, 0, 0 },
+                { 0, 0, 0 }
+            };
+
+            var solution = new _063_UniquePaths2();
+            var result = solution.UniquePathsWithObstacles(input);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void UniquePathsWithObstaclesTest_ObstacleOnFirstColumn()
+        {
+            var input = new int[3, 3]
+            {
+                { 0, 0, 0 },
+                { 1, 0, 0 },
+                { 0, 0, 0 }
+            };
+
+            var solution = new _063_UniquePaths2();
+            var result = solution.UniquePathsWithObstacles(input);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void UniquePathsWithObstaclesTest_ObstacleAtStart()
+        {
+            var input = new int[3, 3]
+            {
+                { 1, 0, 0 },
+                { 0, 0, 0 },
+                { 0, 0, 0 }
+            };
+
+            var solution = new _063_UniquePaths2();
+            var result = solution.UniquePathsWithObstacles(input);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void UniquePathsWithObstaclesTest_ObstacleAtEnd()
+        {
+            var input = new int[3, 3]
+            {
+                { 0, 0, 0 },
+                { 0, 0, 0 },
+                { 0, 0, 1 }
+            };
+
+            var solution = new _063_UniquePaths2();
+            var result = solution.UniquePathsWithObstacles(input);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void UniquePathsWithObstaclesTest_OneItem()
+        {
+            var input = new int[1, 1]
+            {
+                { 0 }
+            };
+
+            var solution = new _063_UniquePaths2();
+            var result = solution.UniquePathsWithObstacles(input);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void UniquePathsWithObstaclesTest_OneItem_Obstacles()
+        {
+            var input = new int[1, 1]
+            {
+                { 1 }
+            };
+
+            var solution = new _063_UniquePaths2();
+            var result = solution.UniquePathsWithObstacles(input);
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ CSharp/LeetCode/063-UniquePaths2.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode
+{
+    public class _063_UniquePaths2
+    {
+        public int UniquePathsWithObstacles(int[,] obstacleGrid)
+        {
+            var rowLenght = obstacleGrid.GetLength(0);
+            var columnLenght = obstacleGrid.GetLength(1);
+            if (obstacleGrid[0, 0] == 1 || obstacleGrid[rowLenght - 1, columnLenght - 1] == 1) { return 0; }
+
+            var possiblePath = new int[rowLenght, columnLenght];
+            possiblePath[0, 0] = 1;
+            for (int i = 1; i < columnLenght; i++)
+            {
+                if (obstacleGrid[0, i] == 0 && possiblePath[0, i - 1] == 1)
+                    possiblePath[0, i] = 1;
+            }
+            for (int i = 1; i < rowLenght; i++)
+            {
+                if (obstacleGrid[i, 0] == 0 && possiblePath[i - 1, 0] == 1)
+                    possiblePath[i, 0] = 1;
+            }
+            for (int i = 1; i < rowLenght; i++)
+            {
+                for (int j = 1; j < columnLenght; j++)
+                {
+                    if (obstacleGrid[i, j] == 0)
+                        possiblePath[i, j] = possiblePath[i - 1, j] + possiblePath[i, j - 1];
+                }
+            }
+
+            return possiblePath[rowLenght - 1, columnLenght - 1];
+        }
+    }
+}
