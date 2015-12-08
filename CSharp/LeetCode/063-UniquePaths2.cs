295b76e2811e35ca9750a63fbe86e5b29627492e
BigEgg <cd_bigegg@hotmail.com>
2015-12-08 21:34:26 +0800

    #63 Use less memories

--- CSharp/LeetCode/063-UniquePaths2.cs
+++ CSharp/LeetCode/063-UniquePaths2.cs
@@ -6,30 +6,17 @@
         {
             var rowLenght = obstacleGrid.GetLength(0);
             var columnLenght = obstacleGrid.GetLength(1);
-            if (obstacleGrid[0, 0] == 1 || obstacleGrid[rowLenght - 1, columnLenght - 1] == 1) { return 0; }
 
-            var possiblePath = new int[rowLenght, columnLenght];
-            possiblePath[0, 0] = 1;
-            for (int i = 1; i < columnLenght; i++)
-            {
-                if (obstacleGrid[0, i] == 0 && possiblePath[0, i - 1] == 1)
-                    possiblePath[0, i] = 1;
-            }
-            for (int i = 1; i < rowLenght; i++)
-            {
-                if (obstacleGrid[i, 0] == 0 && possiblePath[i - 1, 0] == 1)
-                    possiblePath[i, 0] = 1;
-            }
-            for (int i = 1; i < rowLenght; i++)
-            {
-                for (int j = 1; j < columnLenght; j++)
-                {
-                    if (obstacleGrid[i, j] == 0)
-                        possiblePath[i, j] = possiblePath[i - 1, j] + possiblePath[i, j - 1];
-                }
-            }
+            var possiblePath = new int[columnLenght];
+            possiblePath[0] = obstacleGrid[0, 0] == 0 ? 1 : 0;
 
-            return possiblePath[rowLenght - 1, columnLenght - 1];
+            for (int i = 0; i < rowLenght; i++)
+                for (int j = 0; j < columnLenght; j++)
+                    possiblePath[j] = obstacleGrid[i, j] == 1
+                        ? 0
+                        : j != 0 ? possiblePath[j - 1] + possiblePath[j] : possiblePath[j];
+
+            return possiblePath[columnLenght - 1];
         }
     }
 }
