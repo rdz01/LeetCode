e9df551b0d0b05db5be3d6ca0d36a5b2cf07a298
BigEgg <cd_bigegg@hotmail.com>
2015-12-09 22:16:05 +0800

    #64 Add C# solution

---
+++ CSharp/LeetCode.Test/064-MinimumPathSum-Test.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _064_MinimumPathSum_Test
+    {
+        [TestMethod]
+        public void MinPathSumTest()
+        {
+            var input = new int[3, 3]
+            {
+                { 1, 2, 2 },
+                { 0, 1, 1 },
+                { 0, 0, 1 }
+            };
+
+            var solution = new _064_MinimumPathSum();
+            var result = solution.MinPathSum(input);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void MinPathSumTest_OneItem()
+        {
+            var input = new int[1, 1]
+            {
+                { 5 }
+            };
+
+            var solution = new _064_MinimumPathSum();
+            var result = solution.MinPathSum(input);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void MinPathSumTest_RowLarger()
+        {
+            var input = new int[4, 3]
+            {
+                { 1, 2, 2 },
+                { 0, 2, 2 },
+                { 1, 1, 2 },
+                { 0, 1, 1 },
+            };
+
+            var solution = new _064_MinimumPathSum();
+            var result = solution.MinPathSum(input);
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void MinPathSumTest_ColumnLarger()
+        {
+            var input = new int[3, 4]
+            {
+                { 1, 0, 1, 0 },
+                { 2, 2, 1, 1 },
+                { 2, 2, 2, 1 }
+            };
+
+            var solution = new _064_MinimumPathSum();
+            var result = solution.MinPathSum(input);
+
+            Assert.AreEqual(4, result);
+        }
+    }
+}
---
+++ CSharp/LeetCode/064-MinimumPathSum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class _064_MinimumPathSum
+    {
+        public int MinPathSum(int[,] grid)
+        {
+            var rowLenght = grid.GetLength(0);
+            var columnLenght = grid.GetLength(1);
+
+            if (rowLenght <= columnLenght)
+            {
+                var pathSum = new int[rowLenght];
+                for (int i = 0; i < rowLenght; i++)
+                    pathSum[i] = int.MaxValue;
+                pathSum[0] = 0;
+
+                for (int i = 0; i < columnLenght; i++)
+                {
+                    pathSum[0] += grid[0, i];
+                    for (int j = 1; j < rowLenght; j++)
+                        pathSum[j] = Math.Min(pathSum[j - 1], pathSum[j]) + grid[j, i];
+                }
+                return pathSum[rowLenght - 1];
+            }
+            else
+            {
+                var pathSum = new int[columnLenght];
+                for (int i = 0; i < columnLenght; i++)
+                    pathSum[i] = int.MaxValue;
+                pathSum[0] = 0;
+
+                for (int i = 0; i < rowLenght; i++)
+                {
+                    pathSum[0] += grid[i, 0];
+                    for (int j = 1; j < columnLenght; j++)
+                        pathSum[j] = Math.Min(pathSum[j - 1], pathSum[j]) + grid[i, j];
+                }
+
+                return pathSum[columnLenght - 1];
+            }
+        }
+    }
+}
