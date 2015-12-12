b2185849aa763bc911e54dacadfd2585647beb67
BigEgg <cd_bigegg@hotmail.com>
2015-12-12 16:31:54 +0800

    #66 Add C# solution

---
+++ CSharp/LeetCode.Test/066-PlusOne-Test.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _066_PlusOne_Test
+    {
+        [TestMethod]
+        public void PlusOneTest()
+        {
+            var input = new int[] { 1, 2, 3 };
+
+            var solution = new _066_PlusOne();
+            var result = solution.PlusOne(input);
+
+            AssertArray(new int[] { 1, 2, 4 }, result);
+        }
+
+        [TestMethod]
+        public void PlusOneTest_Zero()
+        {
+            var input = new int[] { 0 };
+
+            var solution = new _066_PlusOne();
+            var result = solution.PlusOne(input);
+
+            AssertArray(new int[] { 1 }, result);
+        }
+
+        [TestMethod]
+        public void PlusOneTest_WithCarry()
+        {
+            var input = new int[] { 9, 9, 9 };
+
+            var solution = new _066_PlusOne();
+            var result = solution.PlusOne(input);
+
+            AssertArray(new int[] { 1, 0, 0, 0 }, result);
+        }
+
+
+        void AssertArray(int[] expected, int[] actual)
+        {
+            Assert.AreEqual(expected.Length, actual.Length);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
---
+++ CSharp/LeetCode/066-PlusOne.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class _066_PlusOne
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            var result = new List<int>();
+
+            var carry = 1;
+            var value = 0;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                value = digits[i] + carry;
+                carry = value == 10 ? 1 : 0;
+                result.Insert(0, value % 10);
+            }
+
+            if (carry >= 1)
+            {
+                result.Insert(0, carry);
+            }
+            return result.ToArray();
+        }
+    }
+}
