80f3ae85f703493bf8d8e75a2de9830459299a2c
BigEgg <cd_bigegg@hotmail.com>
2015-12-12 18:07:03 +0800

    #68 Add C# solution

---
+++ CSharp/LeetCode.Test/068-TextJustification-Test.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _068_TextJustification_Test
+    {
+        [TestMethod]
+        public void FullJustifyTest()
+        {
+            var input = new string[] { "This", "is", "an", "example", "of", "text", "justification." };
+
+            var solution = new _068_TextJustification();
+            var result = solution.FullJustify(input, 16);
+
+            AssertStrings(new string[] {
+                "This    is    an",
+                "example  of text",
+                "justification.  "
+            }, result);
+        }
+
+        [TestMethod]
+        public void FullJustifyTest_OneLine()
+        {
+            var input = new string[] { "justification." };
+
+            var solution = new _068_TextJustification();
+            var result = solution.FullJustify(input, 16);
+
+            AssertStrings(new string[] {
+                "justification.  "
+            }, result);
+        }
+
+        [TestMethod]
+        public void FullJustifyTest_JustEnough()
+        {
+            var input = new string[] { "This", "is", "an", "example", "of", "text" };
+
+            var solution = new _068_TextJustification();
+            var result = solution.FullJustify(input, 7);
+
+            AssertStrings(new string[] {
+                "This is",
+                "an     ",
+                "example",
+                "of text"
+            }, result);
+        }
+
+        [TestMethod]
+        public void FullJustifyTest_AlittleLess()
+        {
+            var input = new string[] { "This", "is", "an", "of", "text" };
+
+            var solution = new _068_TextJustification();
+            var result = solution.FullJustify(input, 6);
+
+            AssertStrings(new string[] {
+                "This  ",
+                "is  an",
+                "of    ",
+                "text  "
+            }, result);
+        }
+
+
+        void AssertStrings(IList<string> expected, IList<string> actual)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
---
+++ CSharp/LeetCode/068-TextJustification.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class _068_TextJustification
+    {
+        public IList<string> FullJustify(string[] words, int maxWidth)
+        {
+            var result = new List<string>();
+
+            int start = 0, len = 0, space = 0;
+            var builder = new StringBuilder();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (len + words[i].Length > maxWidth)
+                {
+                    space = maxWidth - len + (i - start);
+                    builder.Clear();
+                    for (int j = start; j < i; j++)
+                    {
+                        builder.Append(words[j]);
+
+                        if (j != i - 1)
+                        {
+                            var tempSpace = space / (i - start - 1) + ((j - start < (space % (i - start - 1))) ? 1 : 0);
+                            builder.Append(new string(' ', Math.Min(maxWidth - builder.Length, tempSpace)));
+                        }
+                        else
+                        {
+                            builder.Append(new string(' ', maxWidth - builder.Length));
+                        }
+                    }
+                    result.Add(builder.ToString());
+
+                    len = 0;
+                    start = i;
+                }
+
+                len += words[i].Length + 1;
+            }
+
+            builder.Clear();
+            for (int j = start; j < words.Length; j++)
+            {
+                builder.Append(words[j]);
+                if (j != words.Length - 1)
+                {
+                    builder.Append(' ');
+                }
+                else
+                {
+                    builder.Append(new string(' ', maxWidth - builder.Length));
+                }
+            }
+            result.Add(builder.ToString());
+
+            return result;
+        }
+    }
+}
