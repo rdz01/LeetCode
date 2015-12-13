700dac82170cdc3dc8c21beed69fb1e4a81152aa
BigEgg <cd_bigegg@hotmail.com>
2015-12-13 20:00:43 +0800

    #70 Add C# solution

---
+++ CSharp/LeetCode/070-ClimbingStairs.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode
+{
+    public class _070_ClimbingStairs
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n < 4) { return n; }
+
+            int x1 = 2, x2 = 3, temp;
+            for (int i = 4; i <= n; i++)
+            {
+                temp = x1 + x2;
+                x1 = x2;
+                x2 = temp;
+            }
+
+            return x2;
+        }
+    }
+}
