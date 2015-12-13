5219d22727a5535c52276bfb4871fa6f922c42c6
BigEgg <cd_bigegg@hotmail.com>
2015-12-13 19:36:27 +0800

    #69 Add C# solution

---
+++ CSharp/LeetCode/069-Sqrt(x).cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode
+{
+    public class _069_Sqrt
+    {
+        public int MySqrt(int x)
+        {
+            if (x < 2) { return x; }
+
+            long value;
+            int start = 0, mid, end = x / 2;
+            while (end >= start)
+            {
+                mid = start + (end - start) / 2;
+                value = mid * (long)mid;
+
+                if (value < x) { start = mid + 1; }
+                else if (value > x) { end = mid - 1; }
+                else { return mid; }
+            }
+
+            return end;
+        }
+    }
+}
