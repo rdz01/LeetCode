004f7d0953063dd0e9ed7a9c7893287f50c603fd
BigEgg <cd_bigegg@hotmail.com>
2015-12-16 22:12:38 +0800

    #71 Add C# solution

---
+++ CSharp/LeetCode/071-SimplifyPath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class _071_SimplifyPath
+    {
+        public string SimplifyPath(string path)
+        {
+            if (string.IsNullOrEmpty(path)) { return "/"; }
+
+            var folders = path.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            var pathList = new List<string>();
+            foreach (var folder in folders)
+            {
+                if (folder.Equals(".")) { continue; }
+                else if (folder.Equals(".."))
+                    if (pathList.Count > 0) { pathList.RemoveAt(pathList.Count - 1); } else { continue; }
+                else { pathList.Add(folder); }
+            }
+
+            var result = "/";
+            if (pathList.Count > 0) { result += string.Join("/", pathList); }
+
+            return result;
+        }
+    }
+}
