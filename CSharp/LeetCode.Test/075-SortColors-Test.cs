d95a4f79afd2b1e09664fe7e071640552092d535
BigEgg <cd_bigegg@hotmail.com>
2015-12-19 00:00:31 +0800

    #75 Add C# solution

---
+++ CSharp/LeetCode.Test/075-SortColors-Test.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _075_SortColors_Test
+    {
+        [TestMethod]
+        public void SortColorsTest()
+        {
+            var input = new int[] { 1, 2, 0, 1, 2, 0, 1, 2, 0 };
+
+            var solution = new _075_SortColors();
+            solution.SortColors(input);
+
+            AssertArray(new int[] { 0, 0, 0, 1, 1, 1, 2, 2, 2 }, input);
+        }
+
+        [TestMethod]
+        public void SortColorsTest_2()
+        {
+            var input = new int[] { 0, 1, 0 };
+
+            var solution = new _075_SortColors();
+            solution.SortColors(input);
+
+            AssertArray(new int[] { 0, 0, 1 }, input);
+        }
+
+        [TestMethod]
+        public void SortColorsTest_OnlyOneItem_Zero()
+        {
+            var input = new int[] { 0 };
+
+            var solution = new _075_SortColors();
+            solution.SortColors(input);
+
+            AssertArray(new int[] { 0 }, input);
+        }
+
+        [TestMethod]
+        public void SortColorsTest_OnlyOneItem_One()
+        {
+            var input = new int[] { 1 };
+
+            var solution = new _075_SortColors();
+            solution.SortColors(input);
+
+            AssertArray(new int[] { 1 }, input);
+        }
+
+        [TestMethod]
+        public void SortColorsTest_OnlyOneItem_Two()
+        {
+            var input = new int[] { 2 };
+
+            var solution = new _075_SortColors();
+            solution.SortColors(input);
+
+            AssertArray(new int[] { 2 }, input);
+        }
+
+        [TestMethod]
+        public void SortColorsTest_AllSame_Zero()
+        {
+            var input = new int[] { 0, 0, 0 };
+
+            var solution = new _075_SortColors();
+            solution.SortColors(input);
+
+            AssertArray(new int[] { 0, 0, 0 }, input);
+        }
+
+        [TestMethod]
+        public void SortColorsTest_AllSame_One()
+        {
+            var input = new int[] { 1, 1, 1 };
+
+            var solution = new _075_SortColors();
+            solution.SortColors(input);
+
+            AssertArray(new int[] { 1, 1, 1 }, input);
+        }
+
+        [TestMethod]
+        public void SortColorsTest_AllSame_Two()
+        {
+            var input = new int[] { 2, 2, 2 };
+
+            var solution = new _075_SortColors();
+            solution.SortColors(input);
+
+            AssertArray(new int[] { 2, 2, 2 }, input);
+        }
+
+
+        void AssertArray(int[] expected, int[] actual)
+        {
+            Assert.AreEqual(expected.Length, actual.Length);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
---
+++ CSharp/LeetCode/075-SortColors.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode
+{
+    public class _075_SortColors
+    {
+        public void SortColors(int[] nums)
+        {
+            int lt = 0, gt = nums.Length - 1, i = 0, temp;
+            while (i <= gt)
+            {
+                if (nums[i] == 0)
+                {
+                    temp = nums[lt];
+                    nums[lt++] = nums[i];
+                    nums[i++] = temp;
+                }
+                else if (nums[i] == 2)
+                {
+                    nums[i] = nums[gt];
+                    nums[gt--] = 2;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+    }
+}
