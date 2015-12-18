3eb032c9afbd159bf2868a933fc925238924eb63
BigEgg <cd_bigegg@hotmail.com>
2015-12-18 23:19:20 +0800

    #74 Add C# solution

---
+++ CSharp/LeetCode/074-SearchA2DMatrix.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode
+{
+    public class _074_SearchA2DMatrix
+    {
+        public bool SearchMatrix(int[,] matrix, int target)
+        {
+            int lo = 0, hi = matrix.GetLength(0) - 1, mid = 0;
+            while (lo <= hi)
+            {
+                mid = lo + (hi - lo) / 2;
+                if (matrix[mid, 0] < target) { lo = mid + 1; }
+                else if (matrix[mid, 0] > target) { hi = mid - 1; }
+                else { return true; }
+            }
+            if (hi == -1) { return false; }
+
+            int rowId = hi;
+            lo = 0;
+            hi = matrix.GetLength(1) - 1;
+            if (matrix[rowId, 0] > target || matrix[rowId, hi] < target)
+            {
+                return false;
+            }
+            while (lo <= hi)
+            {
+                mid = lo + (hi - lo) / 2;
+                if (matrix[rowId, mid] < target) { lo = mid + 1; }
+                else if (matrix[rowId, mid] > target) { hi = mid - 1; }
+                else { return true; }
+            }
+            return false;
+        }
+    }
+}
