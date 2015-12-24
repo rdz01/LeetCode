cd359c8bb6ceabf178bb85a199af58c86dca7eaa
BigEgg <cd_bigegg@hotmail.com>
2015-12-24 21:48:23 +0800

    #78 Add C# solution

---
+++ CSharp/LeetCode.Test/078-Subsets-Test.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _078_Subsets_Test
+    {
+        [TestMethod]
+        public void SubsetsTest()
+        {
+            var solution = new _078_Subsets();
+            var result = solution.Subsets(new int[] { 1, 2, 3 });
+
+            Assert.AreEqual(8, result.Count);
+            AssertList(new int[] { }, result[0]);
+            AssertList(new int[] { 1, 2, 3 }, result[1]);
+            AssertList(new int[] { 1 }, result[2]);
+            AssertList(new int[] { 2 }, result[3]);
+            AssertList(new int[] { 3 }, result[4]);
+            AssertList(new int[] { 1, 2 }, result[5]);
+            AssertList(new int[] { 1, 3 }, result[6]);
+            AssertList(new int[] { 2, 3 }, result[7]);
+        }
+
+        [TestMethod]
+        public void SubsetsTest_NotOrdered()
+        {
+            var solution = new _078_Subsets();
+            var result = solution.Subsets(new int[] { 3, 2, 1 });
+
+            Assert.AreEqual(8, result.Count);
+            AssertList(new int[] { }, result[0]);
+            AssertList(new int[] { 1, 2, 3 }, result[1]);
+            AssertList(new int[] { 1 }, result[2]);
+            AssertList(new int[] { 2 }, result[3]);
+            AssertList(new int[] { 3 }, result[4]);
+            AssertList(new int[] { 1, 2 }, result[5]);
+            AssertList(new int[] { 1, 3 }, result[6]);
+            AssertList(new int[] { 2, 3 }, result[7]);
+        }
+
+        [TestMethod]
+        public void SubsetsTest_EmptyNums()
+        {
+            var solution = new _078_Subsets();
+            var result = solution.Subsets(new int[] { });
+
+            Assert.AreEqual(1, result.Count);
+            AssertList(new int[] { }, result[0]);
+        }
+
+        [TestMethod]
+        public void SubsetsTest_OneItem()
+        {
+            var solution = new _078_Subsets();
+            var result = solution.Subsets(new int[] { 1 });
+
+            Assert.AreEqual(2, result.Count);
+            AssertList(new int[] { }, result[0]);
+            AssertList(new int[] { 1 }, result[1]);
+        }
+
+
+        private void AssertList(int[] expected, IList<int> actual)
+        {
+            Assert.AreEqual(expected.Length, actual.Count);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
---
+++ CSharp/LeetCode/078-Subsets.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class _078_Subsets
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            var results = new List<IList<int>>();
+            results.Add(new List<int>());
+
+            if (nums == null || nums.Length == 0) { return results; }
+
+            Array.Sort(nums);
+            results.Add(new List<int>(nums));
+
+            int n = nums.Length;
+            var select = new bool[n];
+            int i, j, count = 0;
+
+            bool hasNext;
+            for (int k = 1; k < n; k++)
+            {
+                for (i = 0; i < k; i++) { select[i] = true; }
+                for (; i < n; i++) { select[i] = false; }
+
+                while (true)
+                {
+                    var result = new List<int>();
+                    for (i = 0; i < n; i++)
+                        if (select[i]) { result.Add(nums[i]); }
+                    results.Add(result);
+
+                    hasNext = false;
+                    count = 0;
+                    for (i = 0; i < n - 1; i++)
+                    {
+                        if (select[i] && !select[i + 1])
+                        {
+                            select[i + 1] = true;
+                            select[i] = false;
+                            hasNext = true;
+
+                            for (j = 0; j < i; j++)
+                                select[j] = count-- > 0 ? true : false;
+
+                            break;
+                        }
+
+                        if (select[i]) { count++; }
+                    }
+
+                    if (!hasNext) break;
+                }
+            }
+
+            return results;
+        }
+    }
+}
