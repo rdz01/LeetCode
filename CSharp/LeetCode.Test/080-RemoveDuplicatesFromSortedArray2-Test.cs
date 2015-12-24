63c806e0d125d8e335b0cdc6ab0b6ef7ea3cafd3
BigEgg <cd_bigegg@hotmail.com>
2015-12-24 23:36:27 +0800

    #80 Add C# solution

---
+++ CSharp/LeetCode.Test/080-RemoveDuplicatesFromSortedArray2-Test.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _080_RemoveDuplicatesFromSortedArray2_Test
+    {
+        [TestMethod]
+        public void RemoveDuplicatesTest()
+        {
+            var input = new int[] { 1, 1, 2, 2, 2, 3 };
+
+            var solution = new _080_RemoveDuplicatesFromSortedArray2();
+            var result = solution.RemoveDuplicates(input);
+
+            Assert.AreEqual(5, result);
+            AssertArray(new int[] { 1, 1, 2, 2, 3 }, input);
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesTest_2()
+        {
+            var input = new int[] { 1, 1, 1, 2, 2, 3 };
+
+            var solution = new _080_RemoveDuplicatesFromSortedArray2();
+            var result = solution.RemoveDuplicates(input);
+
+            Assert.AreEqual(5, result);
+            AssertArray(new int[] { 1, 1, 2, 2, 3 }, input);
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesTest_EmptyInput()
+        {
+            var solution = new _080_RemoveDuplicatesFromSortedArray2();
+            var result = solution.RemoveDuplicates(new int[] { });
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesTest_OneItem()
+        {
+            var input = new int[] { 1 };
+
+            var solution = new _080_RemoveDuplicatesFromSortedArray2();
+            var result = solution.RemoveDuplicates(input);
+
+            Assert.AreEqual(1, result);
+            AssertArray(new int[] { 1 }, input);
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesTest_TwoItem()
+        {
+            var input = new int[] { 1, 1 };
+
+            var solution = new _080_RemoveDuplicatesFromSortedArray2();
+            var result = solution.RemoveDuplicates(input);
+
+            Assert.AreEqual(2, result);
+            AssertArray(new int[] { 1, 1 }, input);
+        }
+
+        private void AssertArray(int[] expected, int[] actual)
+        {
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
---
+++ CSharp/LeetCode/080-RemoveDuplicatesFromSortedArray2.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode
+{
+    public class _080_RemoveDuplicatesFromSortedArray2
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length <= 2) { return nums.Length; }
+
+            var index = 2;
+            for (int i = 2; i < nums.Length; i++)
+                if (nums[i] != nums[index - 2])
+                    nums[index++] = nums[i];
+
+            return index;
+        }
+    }
+}
