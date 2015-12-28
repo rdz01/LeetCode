b53840af89c2389ebb65454ab9e4fea218e00c85
BigEgg <cd_bigegg@hotmail.com>
2015-12-28 23:15:56 +0800

    #84 Add C# solution

---
+++ CSharp/LeetCode/084-LargestRectangleInHistogram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class _084_LargestRectangleInHistogram
+    {
+        public int LargestRectangleArea(int[] height)
+        {
+            var stack = new Stack<int>();
+            int i = 0, maxArea = 0, topIndex = 0;
+
+            while (i < height.Length)
+            {
+                if (stack.Count == 0 || height[i] > height[stack.Peek()])
+                {
+                    stack.Push(i++);
+                    continue;
+                }
+
+                topIndex = stack.Pop();
+                maxArea = Math.Max(
+                    maxArea,
+                    height[topIndex] * (stack.Count == 0 ? i : i - stack.Peek() - 1));
+            }
+
+            while (stack.Count != 0)
+            {
+                topIndex = stack.Pop();
+                maxArea = Math.Max(
+                    maxArea,
+                    height[topIndex] * (stack.Count == 0 ? i : i - stack.Peek() - 1));
+            }
+
+            return maxArea;
+        }
+    }
+}
