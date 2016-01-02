3e3fa66f306c540f385873d7cd798c07a0eee484
BigEgg <cd_bigegg@hotmail.com>
2016-01-02 21:43:30 +0800

    #85 Add C# solution

---
+++ CSharp/LeetCode.Test/085-MaximalRectangle-Test.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _085_MaximalRectangle_Test
+    {
+        [TestMethod]
+        public void MaximalRectangleTest()
+        {
+            var input = new char[3, 3]
+            {
+                { '1', '1', '1' },
+                { '1', '0', '1' },
+                { '1', '1', '1' }
+            };
+
+            var solution = new _085_MaximalRectangle();
+            var result = solution.MaximalRectangle(input);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void MaximalRectangleTest_2()
+        {
+            var input = new char[5, 5]
+            {
+                { '1', '1', '1', '1', '1' },
+                { '1', '1', '1', '1', '1' },
+                { '1', '0', '1', '1', '1' },
+                { '1', '1', '1', '1', '1' },
+                { '1', '1', '1', '1', '1' }
+            };
+
+            var solution = new _085_MaximalRectangle();
+            var result = solution.MaximalRectangle(input);
+
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void MaximalRectangleTest_3()
+        {
+            var input = new char[2, 2]
+            {
+                { '1', '0' },
+                { '1', '0' }
+            };
+
+            var solution = new _085_MaximalRectangle();
+            var result = solution.MaximalRectangle(input);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void MaximalRectangleTest_OneItem()
+        {
+            var input = new char[1, 1]
+            {
+                { '1' }
+            };
+
+            var solution = new _085_MaximalRectangle();
+            var result = solution.MaximalRectangle(input);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void MaximalRectangleTest_OneItem_Zero()
+        {
+            var input = new char[1, 1]
+            {
+                { '0' }
+            };
+
+            var solution = new _085_MaximalRectangle();
+            var result = solution.MaximalRectangle(input);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void MaximalRectangleTest_OneRow()
+        {
+            var input = new char[1, 3]
+            {
+                { '1', '1', '1' }
+            };
+
+            var solution = new _085_MaximalRectangle();
+            var result = solution.MaximalRectangle(input);
+
+            Assert.AreEqual(3, result);
+        }
+
+    }
+}
---
+++ CSharp/LeetCode/085-MaximalRectangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class _085_MaximalRectangle
+    {
+        public int MaximalRectangle(char[,] matrix)
+        {
+            var rowLenght = matrix.GetLength(0);
+            var columnLenght = matrix.GetLength(1);
+
+            int i = 0, j = 0;
+            var height = new int[columnLenght + 1];
+            for (i = 0; i < columnLenght; i++)
+                height[i] = matrix[0, i] == '1' ? 1 : 0;
+            height[i] = 0;
+
+            i = 0;
+            int maxArea = 0, topIndex = 0;
+            var stack = new Stack<int>();
+            while (i++ < rowLenght)
+            {
+                j = 0;
+                while (j < height.Length)
+                {
+                    if (stack.Count == 0 || height[j] > height[stack.Peek()])
+                    {
+                        stack.Push(j++);
+                        continue;
+                    }
+
+                    topIndex = stack.Pop();
+                    maxArea = Math.Max(
+                        maxArea,
+                        height[topIndex] * (stack.Count == 0 ? j : j - stack.Peek() - 1));
+                }
+                stack.Clear();
+
+                if (i == rowLenght) { return maxArea; }
+                for (j = 0; j < columnLenght; j++)
+                    height[j] = matrix[i, j] == '1' ? height[j] + 1 : 0;
+            }
+
+            return maxArea;
+        }
+    }
+}
